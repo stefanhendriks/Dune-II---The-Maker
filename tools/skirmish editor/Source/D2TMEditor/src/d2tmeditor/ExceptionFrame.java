e69d381ecce3ff3ebd5383fdb8e6d8b9c96665aa
renezwanenburg <renezwanenburg@52bf4e17-6a1c-0410-83a1-9b5866916151>
2009-07-24 20:25:43 +0000

    Added the T2TMEditor sources in the tools\skirmish editor\Source directory.

---
+++ tools/skirmish editor/Source/D2TMEditor/src/d2tmeditor/ExceptionFrame.java
@@ -0,0 +1,63 @@
+package d2tmeditor;
+
+/**
+ *
+ * @author Rene Zwanenburg
+ */
+public class ExceptionFrame extends javax.swing.JFrame {
+
+    public ExceptionFrame() {
+        initComponents();
+    }
+
+    public ExceptionFrame(Throwable t){
+        this();
+
+        String text = "Critical Error! Please post the following stack trace " +
+                "at the forums:\n\n";
+
+        text += t.toString() + "\n";
+
+        for(StackTraceElement e : t.getStackTrace())
+            text += e.toString() + "\n";
+
+        jTextArea1.setText(text);
+    }
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTextArea1 = new javax.swing.JTextArea();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Critical Error");
+
+        jTextArea1.setColumns(20);
+        jTextArea1.setEditable(false);
+        jTextArea1.setLineWrap(true);
+        jTextArea1.setRows(5);
+        jTextArea1.setWrapStyleWord(true);
+        jScrollPane1.setViewportView(jTextArea1);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 400, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 300, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextArea jTextArea1;
+    // End of variables declaration//GEN-END:variables
+
+}
