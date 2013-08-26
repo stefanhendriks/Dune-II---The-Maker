799b8deccdde60cc2c68564c78958fb190c5998b
Stefan Hendriks <stefanhen83@gmail.com>
2013-08-26 21:09:32 +0200

    now all keyboard handling is done with events

--- eventfactory.h
+++ eventfactory.h
@@ -37,13 +37,14 @@ class EventFactory {
     void pushSelectEvent(int x, int y);
     void pushDeselectEvent() { push_event(D2TM_DESELECT); }
     void pushMoveCameraEvent(int direction);
+    void pushQuitEvent() { push_sdl_event(SDL_QUIT, 0, NULL, NULL); }
 
   private:
 
-    bool push_event(int code, void* data1, void* data2) {
+    bool push_sdl_event(int type, int code, void* data1, void *data2) {
       SDL_Event clickEvent;
 
-      clickEvent.type = SDL_USEREVENT;
+      clickEvent.type = type;
       clickEvent.user.code = code;
       clickEvent.user.data1 = data1;
       clickEvent.user.data2 = data2;
@@ -51,6 +52,10 @@ class EventFactory {
       SDL_PushEvent(&clickEvent);
 
       return true;
+    }
+
+    bool push_event(int code, void* data1, void* data2) {
+      return push_sdl_event(SDL_USEREVENT, code, data1, data2);
     }
 
     bool push_event(int code, void* data1) {
--- keyboard.cpp
+++ keyboard.cpp
@@ -1,18 +1,22 @@
+#include <iostream>
 #include "keyboard.h"
+
+using namespace std;
 
 Keyboard::Keyboard() {
   qPressed = false;
 }
 
 void Keyboard::onEvent(SDL_Event* event) {
-  bool keyDown = event->type == SDL_KEYDOWN;
-  switch( event->key.keysym.sym ) {
-     case SDLK_UP: eventFactory.pushMoveCameraEvent(D2TM_CAMERA_MOVE_UP); break;
-     case SDLK_DOWN: eventFactory.pushMoveCameraEvent(D2TM_CAMERA_MOVE_DOWN); break;
-     case SDLK_LEFT: eventFactory.pushMoveCameraEvent(D2TM_CAMERA_MOVE_LEFT); break;
-     case SDLK_RIGHT: eventFactory.pushMoveCameraEvent(D2TM_CAMERA_MOVE_RIGHT); break;
-     case SDLK_q: qPressed = keyDown;
-     default: break;
+  if (event->type == SDL_KEYDOWN) {
+    switch( event->key.keysym.sym ) {
+       case SDLK_UP: eventFactory.pushMoveCameraEvent(D2TM_CAMERA_MOVE_UP); break;
+       case SDLK_DOWN: eventFactory.pushMoveCameraEvent(D2TM_CAMERA_MOVE_DOWN); break;
+       case SDLK_LEFT: eventFactory.pushMoveCameraEvent(D2TM_CAMERA_MOVE_LEFT); break;
+       case SDLK_RIGHT: eventFactory.pushMoveCameraEvent(D2TM_CAMERA_MOVE_RIGHT); break;
+       case SDLK_q: eventFactory.pushQuitEvent();
+       default: break;
+    }
   }
 }
 
