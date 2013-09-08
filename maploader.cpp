c8db14ab7c531e152ac5e19e8d2fd4a92642bbc2
Stefan Hendriks <stefanhen83@gmail.com>
2013-09-08 15:25:15 +0200

    Load map from skirmish INI file.

--- maploader.cpp
+++ maploader.cpp
@@ -1,5 +1,6 @@
 #include <sstream>
 #include <iomanip>
+#include <string>
 
 #include "maploader.h"
 
@@ -23,12 +24,30 @@ void MapLoader::load(std::string file, Map* map) {
 
   cout << "Dimensions are " << width << "x" << height << "." << endl;
 
-  for (int i = 0; i < height; i++) {
-    string key = makeIntKeyWithLeadingZero(i);
+  for (int y = 0; y < height; y++) {
+    string key = makeIntKeyWithLeadingZero((y + 1));
     string value = reader.Get("DATA", key, "!!!UNKNOWN DATA!!!");
-    cout << "Line with key " << key << " has value [" << value << "]" << endl;
 
+    if (value.at(0) == '!') {
+      cerr << "ERROR: Expected to have data for key " << key << " but none was found." << endl;
+      continue;
+    }
 
+    for (int x = 0; x < width; x++) {
+
+      if (x >= value.length()) {
+        cerr << "ERROR: While parsing line (key=" << key << ") " << value << ":" << endl;
+        cerr << "Line was expected to have width " << width << " but is actually " << value.length() << ". Either width/height property is wrong in [SKIRMISH] section or [DATA] has wrong amount of data in this line." << endl;
+        continue;
+      }
+
+      char c = value.at(x);
+      if (c == 'R') map->getCell(x, y)->terrain_type = TERRAIN_TYPE_MOUNTAIN;
+      if (c == '^') map->getCell(x, y)->terrain_type = TERRAIN_TYPE_ROCK;
+      if (c == ')') map->getCell(x, y)->terrain_type = TERRAIN_TYPE_SAND;
+      if (c == '-') map->getCell(x, y)->terrain_type = TERRAIN_TYPE_SPICE;
+      if (c == '+') map->getCell(x, y)->terrain_type = TERRAIN_TYPE_SPICEHILL;
+    }
   }
 
 }
