3911838cfe032f2f4306fcc36717ecd0304782cd
Stefan Hendriks <stefanhen83@gmail.com>
2013-09-26 20:23:06 +0200

    SDL dies, SFML lives!

---
+++ sfml.cpp
@@ -0,0 +1,24 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+  sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+  sf::CircleShape shape(100.f);
+  shape.setFillColor(sf::Color::Green);
+
+  while (window.isOpen())
+  {
+    sf::Event event;
+    while (window.pollEvent(event))
+    {
+      if (event.type == sf::Event::Closed)
+        window.close();
+    }
+
+    window.clear();
+    window.draw(shape);
+    window.display();
+  }
+
+  return 0;
+}
