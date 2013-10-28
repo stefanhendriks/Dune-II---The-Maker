207d6715c433393d82e34a1cb8ed61db0e8d080e
Stefan Hendriks <stefanhen83@gmail.com>
2013-10-28 20:46:32 +0100

    indenting

--- Console.cpp
+++ Console.cpp
@@ -3,101 +3,103 @@
 #include <boost/algorithm/string.hpp>
 
 Console::Console(MessageSystem& messages, const std::vector<Player> &players):
-    chatLayout(sfg::Box::Create(sfg::Box::VERTICAL, 5.f)),
-    chatAreaLayout(sfg::Box::Create(sfg::Box::VERTICAL)),
-    chatEntry(sfg::Entry::Create()),
-    chatWindow(sfg::ScrolledWindow::Create()),
-    window(sfg::Window::Create(sfg::Window::SHADOW)),
-    messages(messages), players(players)
+  chatLayout(sfg::Box::Create(sfg::Box::VERTICAL, 5.f)),
+  chatAreaLayout(sfg::Box::Create(sfg::Box::VERTICAL)),
+  chatEntry(sfg::Entry::Create()),
+  chatWindow(sfg::ScrolledWindow::Create()),
+  window(sfg::Window::Create(sfg::Window::SHADOW)),
+  messages(messages), players(players)
 {
-    chatWindow->SetRequisition(sf::Vector2f(750.f,100.f));
-    chatWindow->SetScrollbarPolicy( sfg::ScrolledWindow::HORIZONTAL_AUTOMATIC | sfg::ScrolledWindow::VERTICAL_AUTOMATIC );
-    chatWindow->AddWithViewport(chatAreaLayout);
+  chatWindow->SetRequisition(sf::Vector2f(750.f,100.f));
+  chatWindow->SetScrollbarPolicy( sfg::ScrolledWindow::HORIZONTAL_AUTOMATIC | sfg::ScrolledWindow::VERTICAL_AUTOMATIC );
+  chatWindow->AddWithViewport(chatAreaLayout);
 
-    chatLayout->Pack(chatWindow);
-    chatLayout->Pack(chatEntry);
+  chatLayout->Pack(chatWindow);
+  chatLayout->Pack(chatEntry);
 
-    chatEntry->GetSignal(sfg::Entry::OnKeyPress).Connect(&Console::dataReady, this);
+  chatEntry->GetSignal(sfg::Entry::OnKeyPress).Connect(&Console::dataReady, this);
 
-    window->Add(chatLayout);
-    window->Show(false);
+  window->Add(chatLayout);
+  window->Show(false);
 
-    desktop.LoadThemeFromFile("gui.theme");
+  desktop.LoadThemeFromFile("gui.theme");
 
 
-    desktop.Add(window);
-    chatEntry->GrabFocus();
+  desktop.Add(window);
+  chatEntry->GrabFocus();
 
-    unitMap["trike"] = Unit::Type::Trike;
-    unitMap["quad"] = Unit::Type::Quad;
-    unitMap["devastator"] = Unit::Type::Devastator;
-    unitMap["carryall"] = Unit::Type::Carryall;
-    unitMap["frigate"] = Unit::Type::Frigate;
-    unitMap["soldier"] = Unit::Type::Soldier;
+  unitMap["trike"] = Unit::Type::Trike;
+  unitMap["quad"] = Unit::Type::Quad;
+  unitMap["devastator"] = Unit::Type::Devastator;
+  unitMap["carryall"] = Unit::Type::Carryall;
+  unitMap["frigate"] = Unit::Type::Frigate;
+  unitMap["soldier"] = Unit::Type::Soldier;
 }
 
 void Console::update(sf::Time dt)
 {
-    desktop.Update(dt.asSeconds());
+  desktop.Update(dt.asSeconds());
 }
 
 void Console::handleEvent(sf::Event event)
 {
-    desktop.HandleEvent(event);
+  desktop.HandleEvent(event);
 }
 
 void Console::display(sf::RenderWindow &screen)
 {
-    sfgui.Display(screen);
+  sfgui.Display(screen);
 }
 
 void Console::toggle()
 {
-    window->Show(!window->IsLocallyVisible());
+  window->Show(!window->IsLocallyVisible());
 }
 
 void Console::dataReady()
 {
-    if (!sf::Keyboard::isKeyPressed(sf::Keyboard::Return)) return; //this is cheap
-    std::string toProcess(chatEntry->GetText());
-    if (toProcess.empty()) return;
-    chatEntry->SetText("");
+  if (!sf::Keyboard::isKeyPressed(sf::Keyboard::Return)) return; //this is cheap
+  std::string toProcess(chatEntry->GetText());
+  if (toProcess.empty()) return;
+  chatEntry->SetText("");
 
-    std::vector<std::string> tokens;
-    boost::split(tokens, toProcess, boost::is_any_of(" "));
+  std::vector<std::string> tokens;
+  boost::split(tokens, toProcess, boost::is_any_of(" "));
 
-    if (tokens[0]=="add"){
-        if (tokens.size() != 5){
-            toConsole("Incorrect number of arguments.");
-            return;
-        }
-
-        messages.triggerEvent(CreateUnitMessage(unitMap[tokens[1]], players[std::stoi(tokens[2])],
-          {std::stof(tokens[3]), std::stof(tokens[4])}));
-
-        toConsole("Unit added.");
-    }else{
-        toConsole("Bad command.");
-        return;
+  if (tokens[0]=="add"){
+    if (tokens.size() != 5){
+      toConsole("Incorrect number of arguments.");
+      return;
     }
+
+    messages.triggerEvent(
+          CreateUnitMessage(unitMap[tokens[1]],
+        players[std::stoi(tokens[2])],
+    {std::stof(tokens[3]), std::stof(tokens[4])}));
+
+    toConsole("Unit added.");
+  } else {
+    toConsole("Bad command.");
+    return;
+  }
 }
 
 void Console::toConsole(std::string toWrite)
 {
-    sfg::Container::WidgetsList lines(chatAreaLayout->GetChildren());
-    if (lines.size()>120) //if more, pop front
-        chatAreaLayout->Remove(lines.front());
+  sfg::Container::WidgetsList lines(chatAreaLayout->GetChildren());
+  if (lines.size() > 120) //if more, pop front
+    chatAreaLayout->Remove(lines.front());
 
 
-    sfg::Label::Ptr toAdd(sfg::Label::Create(toWrite));
-    toAdd->SetAlignment(sf::Vector2f(0.f,0.f));
-    chatAreaLayout->PackEnd(toAdd);
-    autoscroll();
+  sfg::Label::Ptr toAdd(sfg::Label::Create(toWrite));
+  toAdd->SetAlignment(sf::Vector2f(0.f,0.f));
+  chatAreaLayout->PackEnd(toAdd);
+  autoscroll();
 }
 
 void Console::autoscroll()
 {
-    sfg::Adjustment::Ptr toAdjust(chatWindow->GetVerticalAdjustment());
-    toAdjust->SetValue(toAdjust->GetUpper());
+  sfg::Adjustment::Ptr toAdjust(chatWindow->GetVerticalAdjustment());
+  toAdjust->SetValue(toAdjust->GetUpper());
 }
 
