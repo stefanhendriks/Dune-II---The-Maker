07903de01da2b7e453d3d3668c4304fb3fc37323
Stefan Hendriks <stefan@kaizenbv.nl>
2021-09-04 15:01:05 +0200

    Fix compile warning

--- data/cAllegroDataRepository.h
+++ data/cAllegroDataRepository.h
@@ -24,7 +24,7 @@ public:
      * @return
      */
     int loadBitmap(char *file);
-    bool loadBitmapAt(int index, char *file);
+    bool loadBitmapAt(int index, const char *file);
     bool loadBitmapFromDataFileAt(int index, BITMAP *bmp);
     bool loadBitmapFromDataFileGfxDataAt(int index, int indexAtDataFile);
 
