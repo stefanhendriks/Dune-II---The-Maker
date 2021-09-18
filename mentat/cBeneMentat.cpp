ac2719e894090cd31f8e188b857c253862c56a23
Stefan Hendriks <stefan@kaizenbv.nl>
2021-09-18 15:31:31 +0200

    Draw housename using function

--- mentat/cBeneMentat.cpp
+++ mentat/cBeneMentat.cpp
@@ -21,14 +21,7 @@ void cBeneMentat::draw() {
     // when not speaking, draw 'do you wish to join house x'
     if (state == AWAITING_RESPONSE) {
         alfont_set_font_size(font, 35); // set size
-        std::string houseName = "Unknown";
-        if (house == HARKONNEN) {
-            houseName = "Harkonnen";
-        } else if (house == ATREIDES) {
-            houseName = "Atreides";
-        } else if (house == ORDOS) {
-            houseName = "Ordos";
-        }
+        std::string houseName = cPlayer::getHouseNameForId(house);
         alfont_textprintf_aa_ex(bmp_screen, font, offsetX + 17, offsetY + 17, makecol(0, 0, 0), makecol(0, 0, 0),
                                 "Do you wish to join house %s ?", houseName.c_str());
         alfont_textprintf_aa_ex(bmp_screen, font, offsetX + 16, offsetY + 16, makecol(255, 214, 137), makecol(0, 0, 0),
