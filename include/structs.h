2edb46ac3e69df4bdeb4c1c7336fc2385ac043d3
Stefan Hendriks <stefan@kaizenbv.nl>
2021-09-22 21:54:36 +0200

    Use additive blending by property, not conditional
    - for #328

--- include/structs.h
+++ include/structs.h
@@ -202,6 +202,7 @@ struct s_ParticleInfo {
 
     int startAlpha;         // 0-255 (anything else == 255 = opaque)
 
+    bool usesAdditiveBlending; // type of blending used when alpha > 0 and < 255 (default = transparent, if this is set to false)
 };
 
 struct s_BulletInfo {
