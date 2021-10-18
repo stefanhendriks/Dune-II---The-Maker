afa40b9c81f7b61d3bc58917a9ddfcb278a5079a
Stefan Hendriks <stefan@kaizenbv.nl>
2021-10-18 21:05:45 +0200

    rename param

--- gui.cpp
+++ gui.cpp
@@ -76,21 +76,21 @@ bool GUI_DRAW_BENE_TEXT_MOUSE_SENSITIVE(int x, int y, const std::string& text, i
     return false;	
 }
 
-bool GUI_DRAW_FRAME(int x1, int y1, int width, int height) {
-    cRectangle rect = cRectangle(x1, y1, width, height);
+bool GUI_DRAW_FRAME(int x, int y, int width, int height) {
+    cRectangle rect = cRectangle(x, y, width, height);
     allegroDrawer->gui_DrawRect(bmp_screen, rect);
 
 //    // fill it up
-//    rectfill(bmp_screen, x1, y1, x1+width, y1+height, makecol(176,176,196));
+//    rectfill(bmp_screen, x, y, x+width, y+height, makecol(176,176,196));
 //
 //    // rect
-//    rect(bmp_screen, x1,y1,x1+width, y1+height, makecol(252,252,252));
+//    rect(bmp_screen, x,y,x+width, y+height, makecol(252,252,252));
 //
 //    // lines to darken the right sides
-//    line(bmp_screen, x1+width, y1, x1+width , y1+height, makecol(84,84,120));
-//    line(bmp_screen, x1, y1+height, x1+width , y1+height, makecol(84,84,120));
+//    line(bmp_screen, x+width, y, x+width , y+height, makecol(84,84,120));
+//    line(bmp_screen, x, y+height, x+width , y+height, makecol(84,84,120));
 
-    if ((mouse_x >= x1 && mouse_x < (x1+width)) && (mouse_y >= y1 && mouse_y <= (y1+height))) {
+    if ((mouse_x >= x && mouse_x < (x + width)) && (mouse_y >= y && mouse_y <= (y + height))) {
         return true;
     }
 
