17bf6bf81823f0a80e9b1c9dc4db60880fed56a5
Stefan Hendriks <stefan@kaizenbv.nl>
2021-11-08 23:02:03 +0100

    Use new starport gfx with flag
    - for #348

--- gameobjects/structures/cStarPort.cpp
+++ gameobjects/structures/cStarPort.cpp
@@ -1,39 +1,36 @@
 #include "../../include/d2tmh.h"
 
 // Constructor
-cStarPort::cStarPort()
-{
- // other variables (class specific)
- TIMER_deploy = 0;
- frigateDroppedPackage = false;
+cStarPort::cStarPort() {
+    // other variables (class specific)
+    TIMER_deploy = 0;
+    frigateDroppedPackage = false;
 }
 
 int cStarPort::getType() const {
-	return STARPORT;
+    return STARPORT;
 }
 
-cStarPort::~cStarPort()
-{
+cStarPort::~cStarPort() {
 
 }
 
-void cStarPort::think()
-{
-	// think like base class
-	cAbstractStructure::think();
+void cStarPort::think() {
+    // think like base class
+    cAbstractStructure::think();
 
 }
 
 // think about units deployment animation
 void cStarPort::think_deployment() {
-	if (!isAnimating()) {
-		return; // do nothing when not animating
-	}
+    if (!isAnimating()) {
+        return; // do nothing when not animating
+    }
 
-	if (!frigateDroppedPackage) {
-		if (iFrame < 0) {
+    if (!frigateDroppedPackage) {
+        if (iFrame < 0) {
             iFrame = 1;
-		}
+        }
 
         TIMER_flag++;
 
@@ -41,67 +38,67 @@ void cStarPort::think_deployment() {
             TIMER_flag = 0;
 
             iFrame++;
-			if (iFrame > 3) {
-				iFrame = 1;
-			}
+            if (iFrame > 3) {
+                iFrame = 1;
+            }
         }
-	} else {
-		// show frame with the package
+    } else {
+        // show frame with the package
         iFrame = 4;
-	}
+    }
 }
 
 // Specific Animation thinking (flag animation OR its deploy animation)
 void cStarPort::think_animation() {
-	cAbstractStructure::think_animation();
-	cAbstractStructure::think_flag();
-	think_deployment();
+    cAbstractStructure::think_animation();
+    cAbstractStructure::think_flag_new();
+    think_deployment();
 }
 
 // FPS based thing function (every second called)
-void cStarPort::think_deploy()
-{
-	if (frigateDroppedPackage) {
-		  TIMER_deploy--;
-		  if (TIMER_deploy < 0) {
-			  TIMER_deploy = 1;
-			  // deploy unit
-			  cOrderProcesser * orderProcesser = players[iPlayer].getOrderProcesser();
-			  cBuildingListItem * item = orderProcesser->getItemToDeploy();
-			  if (item) {
-				  int cellToDeployTo = getNonOccupiedCellAroundStructure();
-				  orderProcesser->markOrderAsDeployed(item);
-				  item->decreaseTimesOrdered();
-				  int rallyPoint = getRallyPoint();
+void cStarPort::think_deploy() {
+    if (frigateDroppedPackage) {
+        TIMER_deploy--;
+        if (TIMER_deploy < 0) {
+            TIMER_deploy = 1;
+            // deploy unit
+            cOrderProcesser *orderProcesser = players[iPlayer].getOrderProcesser();
+            cBuildingListItem *item = orderProcesser->getItemToDeploy();
+            if (item) {
+                int cellToDeployTo = getNonOccupiedCellAroundStructure();
+                orderProcesser->markOrderAsDeployed(item);
+                item->decreaseTimesOrdered();
+                int rallyPoint = getRallyPoint();
 
-				  if (cellToDeployTo >= 0) {
-					  int id = UNIT_CREATE(cellToDeployTo, item->getBuildId(), iPlayer, true);
-					  if (rallyPoint > -1) {
-						  unit[id].move_to(rallyPoint, -1, -1);
-					  }
-					  play_voice(SOUND_VOICE_05_ATR); // unit deployed
-				  } else {
-					  // could not find cell to deploy to, reinforce it
-					  if (rallyPoint > -1) {
-						  cellToDeployTo = rallyPoint;
-					  }
+                if (cellToDeployTo >= 0) {
+                    int id = UNIT_CREATE(cellToDeployTo, item->getBuildId(), iPlayer, true);
+                    if (rallyPoint > -1) {
+                        unit[id].move_to(rallyPoint, -1, -1);
+                    }
+                    play_voice(SOUND_VOICE_05_ATR); // unit deployed
+                } else {
+                    // could not find cell to deploy to, reinforce it
+                    if (rallyPoint > -1) {
+                        cellToDeployTo = rallyPoint;
+                    }
 
-					  if (cellToDeployTo < 0) {
-						  // in this case, the structure has nothing around it free, but nor a rally point is set
-						  // assume that the cell to drop is the location of the structure itself
-						  cellToDeployTo = getCell();
-					  }
-					  int cellAtBorderOfMap = map.findCloseMapBorderCellRelativelyToDestinationCel(cellToDeployTo);
-					  REINFORCE(iPlayer, item->getBuildId(), cellToDeployTo, cellAtBorderOfMap);
-				  }
-			  } else {
-				  // item is null, no more items to deploy.
-				  setAnimating(false);
-				  orderProcesser->setOrderHasBeenProcessed();
-				  frigateDroppedPackage = false;
-			  }
-		  }
-	}
+                    if (cellToDeployTo < 0) {
+                        // in this case, the structure has nothing around it free, but nor a rally point is set
+                        // assume that the cell to drop is the location of the structure itself
+                        cellToDeployTo = getCell();
+                    }
+                    int cellAtBorderOfMap = map.findCloseMapBorderCellRelativelyToDestinationCel(cellToDeployTo);
+                    REINFORCE(iPlayer, item->getBuildId(), cellToDeployTo, cellAtBorderOfMap);
+                }
+            } else {
+                // item is null, no more items to deploy.
+                setAnimating(false);
+                orderProcesser->setOrderHasBeenProcessed();
+                iFrame = 0;
+                frigateDroppedPackage = false;
+            }
+        }
+    }
 }
 
 void cStarPort::think_guard() {
