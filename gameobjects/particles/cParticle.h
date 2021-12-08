714d5d0b629de9006e892166845653002f03bd70
Stefan Hendriks <stefan@kaizenbv.nl>
2021-12-08 17:07:16 +0100

    Make sure that particle moves with unit
    - shadow particle is bound to smoke particle (so it can move along with the unit)
    - when particle dies, it gets detached from unit, so unit can spawn another smoke particle if required
    - for #274

--- gameobjects/particles/cParticle.h
+++ gameobjects/particles/cParticle.h
@@ -37,10 +37,10 @@ public:
 
     void bindToUnit(int unitID);
 
-    void die() {
-        bindToUnit(-1);
-        bAlive = true;
-    }
+    void die();
+
+    void addPosX(float d);
+    void addPosY(float d);
 
 private:
     int getFrameWidth();
@@ -51,7 +51,7 @@ private:
     // x and y position to draw (absolute numbers).
     // This is the 'center' of a particle from a drawing perspective. Meaning the center of the
     // bitmap rendered is at this x,y coordinate
-    long x, y;
+    float x, y;
 
     // the actual position + dimensions of particle
     cRectangle * dimensions;
@@ -71,7 +71,7 @@ private:
     int TIMER_frame;    // frame animation timers (when < 0, next frame, etc)
     // when TIMER_dead < 0, the last frame lets this thing die
 
-    int TIMER_dead;     // when > -1, this timer will determine when the thing dies
+    int TIMER_dead;     // when > -1, this timer will determine when the particle dies
 
     int iHousePal;      // when specified, use this palette for drawing (and its an 8 bit picture then!)
 
@@ -81,6 +81,7 @@ private:
     bool oldParticle;
 
     int boundUnitID;
+    int boundParticleID; // when particles are 'tied together'
 
     int draw_x();
 
