91f0c877a8552359dafecc26bd113fd956fa67f7
Stefan Hendriks <stefan@kaizenbv.nl>
2021-12-27 16:47:02 +0100

    Add a "repair" state, instead of making it a 'substate' of normal mouse
    - when you leave repair state, you get back to the previous state
    - made it explicit from which state you can get to repair state

---
+++ controls/mousestates/cMouseRepairState.cpp
@@ -0,0 +1,98 @@
+#include <algorithm>
+#include "d2tmh.h"
+
+#include "cMouseRepairState.h"
+
+cMouseRepairState::cMouseRepairState(cPlayer *player, cGameControlsContext *context, cMouse *mouse) :
+        cMouseState(player, context, mouse) {
+
+}
+
+void cMouseRepairState::onNotifyMouseEvent(const s_MouseEvent &event) {
+    // these methods can have a side-effect which changes mouseTile...
+    switch (event.eventType) {
+        case MOUSE_LEFT_BUTTON_CLICKED:
+            onMouseLeftButtonClicked(event);
+            break;
+        case MOUSE_RIGHT_BUTTON_PRESSED:
+            onMouseRightButtonPressed(event);
+            break;
+        case MOUSE_RIGHT_BUTTON_CLICKED:
+            onMouseRightButtonClicked(event);
+            break;
+        case MOUSE_MOVED_TO:
+            onMouseMovedTo(event);
+            break;
+        default:
+            break;
+    }
+
+    // ... so set it here
+    if (context->isState(MOUSESTATE_REPAIR)) { // if , required in case we switched state
+        mouse->setTile(mouseTile);
+    }
+}
+
+cMouseRepairState::~cMouseRepairState() {
+
+}
+
+void cMouseRepairState::onMouseLeftButtonClicked(const s_MouseEvent &event) {
+    int hoverUnitId = context->getIdOfUnitWhereMouseHovers();
+    if (hoverUnitId > -1) {
+        cUnit &pUnit = unit[hoverUnitId];
+        if (pUnit.isValid() && pUnit.belongsTo(player) && pUnit.isEligibleForRepair()) {
+            pUnit.findBestStructureCandidateAndHeadTowardsItOrWait(REPAIR, true);
+        }
+    }
+
+    cAbstractStructure *pStructure = context->getStructurePointerWhereMouseHovers();
+    if (pStructure && pStructure->isValid()) {
+        if (pStructure->belongsTo(player) && pStructure->isDamaged()) {
+            pStructure->setRepairing(!pStructure->isRepairing());
+        }
+    }
+}
+
+void cMouseRepairState::onMouseRightButtonPressed(const s_MouseEvent &event) {
+    // NOOP
+}
+
+void cMouseRepairState::onMouseRightButtonClicked(const s_MouseEvent &event) {
+    context->toPreviousState();
+}
+
+void cMouseRepairState::onMouseMovedTo(const s_MouseEvent &event) {
+    mouseTile = getMouseTileForRepairState();
+}
+
+void cMouseRepairState::onStateSet() {
+    mouseTile = getMouseTileForRepairState();
+    mouse->setTile(mouseTile);
+}
+
+
+void cMouseRepairState::onNotifyKeyboardEvent(const s_KeyboardEvent &event) {
+    if (event.eventType == KEY_PRESSED && event.key == KEY_R) {
+        context->toPreviousState();
+    }
+}
+
+int cMouseRepairState::getMouseTileForRepairState() {
+    int hoverUnitId = context->getIdOfUnitWhereMouseHovers();
+    if (hoverUnitId > -1) {
+        cUnit &pUnit = unit[hoverUnitId];
+        if (pUnit.isValid() && pUnit.belongsTo(player) && pUnit.isEligibleForRepair()) {
+            return MOUSE_REPAIR;
+        }
+    }
+
+    cAbstractStructure *pStructure = context->getStructurePointerWhereMouseHovers();
+    if (pStructure && pStructure->isValid()) {
+        if (pStructure->belongsTo(player) && pStructure->isDamaged()) {
+            return MOUSE_REPAIR;
+        }
+    }
+
+    return MOUSE_FORBIDDEN;
+}
---
+++ controls/mousestates/cMouseRepairState.h
@@ -0,0 +1,34 @@
+#ifndef D2TM_CMOUSEREPAIRSTATE_H
+#define D2TM_CMOUSEREPAIRSTATE_H
+
+#include "cMouseState.h"
+
+/**
+ * In this state the mouse can repair (or order to repair) things.
+ *
+ */
+class cMouseRepairState : public cMouseState {
+
+public:
+    explicit cMouseRepairState(cPlayer * player, cGameControlsContext *context, cMouse * mouse);
+    ~cMouseRepairState() override;
+
+    void onNotifyMouseEvent(const s_MouseEvent &event) override;
+    void onNotifyKeyboardEvent(const s_KeyboardEvent &event) override;
+    void onStateSet() override;
+
+private:
+    void onMouseLeftButtonClicked(const s_MouseEvent &event);
+
+    void onMouseRightButtonPressed(const s_MouseEvent &event);
+
+    void onMouseRightButtonClicked(const s_MouseEvent &event);
+
+    void onMouseMovedTo(const s_MouseEvent &event);
+
+    int getMouseTileForRepairState();
+
+};
+
+
+#endif //D2TM_CMOUSEREPAIRSTATE_H
