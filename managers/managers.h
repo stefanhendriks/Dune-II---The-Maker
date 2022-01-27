ccea412adf6ce9e9644afbe8a6d4415dde3693d2
Stefan Hendriks <stefan@fundynamic.com>
2022-01-27 22:39:52 +0100

    Get rid of keyboard manager
    - start responding to keyboard event

--- managers/managers.h
+++ managers/managers.h
@@ -1,17 +1,5 @@
-/*
- * managerh.h
- *
- *  Created on: 10-aug-2010
- *      Author: Stefan
- */
-
-#ifndef MANAGERH_H_
-#define MANAGERH_H_
-
+#pragma once
 class cPlayer;
 
 #include "cDrawManager.h"
-#include "cKeyboardManager.h"
 #include "cInteractionManager.h"
-
-#endif /* MANAGERH_H_ */
