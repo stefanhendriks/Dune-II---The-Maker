60921d8bc4c84526338cc386e2731ee0a93dc25c
Stefan Hendriks <stefan@fundynamic.com>
2022-01-30 23:49:06 +0100

    Make all member fields use m_
    - and mention the person who triggered me into working on D2TM again

--- gamestates/cCreditsState.cpp
+++ gamestates/cCreditsState.cpp
@@ -4,25 +4,26 @@
 #include <alfont.h>
 #include <allegro.h>
 
-cCreditsState::cCreditsState(cGame &theGame) : cGameState(theGame), textDrawer(cTextDrawer(bene_font)) {
-    moveSpeed = 0.15f;
+cCreditsState::cCreditsState(cGame &theGame) :
+    cGameState(theGame),
+    m_moveSpeed(0.15f),
+    m_textDrawer(cTextDrawer(bene_font)),
+    m_duneBmp((BITMAP *) gfxinter[BMP_GAME_DUNE].dat),
+    m_titleBmp((BITMAP *) gfxinter[BMP_D2TM].dat) {
 
-    bmp_D2TM_Title = (BITMAP *) gfxinter[BMP_D2TM].dat;
-    bmp_Dune = (BITMAP *) gfxinter[BMP_GAME_DUNE].dat;
+    int duneAtTheRight = game.m_screenX - (m_duneBmp->w * 1.1f);
+    int duneAlmostAtBottom = game.m_screenY - (m_duneBmp->h * 1.1f);
+    m_duneCoordinates = cPoint(duneAtTheRight, duneAlmostAtBottom);
 
-    int duneAtTheRight = game.m_screenX - (bmp_Dune->w * 1.1f);
-    int duneAlmostAtBottom = game.m_screenY - (bmp_Dune->h * 1.1f);
-    coords_Dune = cPoint(duneAtTheRight, duneAlmostAtBottom);
-
-    int logoWidth = bmp_D2TM_Title->w;
-    logoHeight = bmp_D2TM_Title->h;
+    int titleWidth = m_titleBmp->w;
+    m_titleHeight = m_titleBmp->h;
 
     int centerOfScreen = game.m_screenX / 2;
 
-    logoX = centerOfScreen - (logoWidth / 2);
+    m_titleX = centerOfScreen - (titleWidth / 2);
     resetCrawler();
 
-    lines = std::vector<s_CreditLine>();
+    m_lines = std::vector<s_CreditLine>();
     prepareCrawlerLines();
 }
 
@@ -34,217 +35,222 @@ void cCreditsState::prepareCrawlerLines() {
 
     int colorHeadlines = colorGreen;
 
-    int fontHeightWithALittlePadding = textDrawer.getFontHeight() + 4;
-    int spacerAfterHeadline = fontHeightWithALittlePadding + textDrawer.getFontHeight();
+    int fontHeightWithALittlePadding = m_textDrawer.getFontHeight() + 4;
+    int spacerAfterHeadline = fontHeightWithALittlePadding + m_textDrawer.getFontHeight();
     auto spacer = s_CreditLine {
         .txt = "",
         .color = makecol(0,0,0),
         .height = 40
     };
 
-    lines.push_back(s_CreditLine {
+    m_lines.push_back(s_CreditLine {
         .txt = fmt::format("Version {}", game.m_version),
         .color = colorYellow,
         .height = spacerAfterHeadline
     });
-    lines.push_back(s_CreditLine {
+    m_lines.push_back(s_CreditLine {
             .txt = "by Stefan Hendriks",
             .color = colorWhite,
             .height = fontHeightWithALittlePadding
     });
-    lines.push_back(spacer);
-    lines.push_back(s_CreditLine {
+    m_lines.push_back(spacer);
+    m_lines.push_back(s_CreditLine {
         .txt = "a remake of",
         .color = colorWhite,
         .height = spacerAfterHeadline
     });
-    lines.push_back(s_CreditLine {
+    m_lines.push_back(s_CreditLine {
         .txt = "Dune 2 - The Building of a Dynasty (by Westwood Studios)",
         .color = colorRed,
         .height = spacerAfterHeadline
     });
 
-    lines.push_back(spacer);
+    m_lines.push_back(spacer);
 
-    lines.push_back(s_CreditLine {
+    m_lines.push_back(s_CreditLine {
             .txt = "--- CONTRIBUTORS ---",
             .color = colorHeadlines,
             .height = spacerAfterHeadline,
     });
-    lines.push_back(s_CreditLine {
+    m_lines.push_back(s_CreditLine {
             .txt = "Paul - Developer",
             .color = colorWhite,
             .height = fontHeightWithALittlePadding
     });
-    lines.push_back(s_CreditLine {
+    m_lines.push_back(s_CreditLine {
             .txt = "Arjen - Developer & Sparring partner",
             .color = colorWhite,
             .height = fontHeightWithALittlePadding
     });
-    lines.push_back(s_CreditLine {
+    m_lines.push_back(s_CreditLine {
             .txt = "Rozmy - Graphics",
             .color = colorWhite,
             .height = fontHeightWithALittlePadding
     });
-    lines.push_back(s_CreditLine {
+    m_lines.push_back(s_CreditLine {
             .txt = "Nema Fakei - Graphics",
             .color = colorWhite,
             .height = fontHeightWithALittlePadding
     });
-    lines.push_back(s_CreditLine {
+    m_lines.push_back(s_CreditLine {
             .txt = "Vidiware - Graphics",
             .color = colorWhite,
             .height = fontHeightWithALittlePadding
     });
-    lines.push_back(s_CreditLine {
+    m_lines.push_back(s_CreditLine {
             .txt = "Timenn - Graphics",
             .color = colorWhite,
             .height = fontHeightWithALittlePadding
     });
-    lines.push_back(s_CreditLine {
+    m_lines.push_back(s_CreditLine {
             .txt = "MrFlibble - Graphics",
             .color = colorWhite,
             .height = fontHeightWithALittlePadding
     });
-    lines.push_back(s_CreditLine {
+    m_lines.push_back(s_CreditLine {
             .txt = "Ripps - Support at Discord, testing & Skirmish Maps",
             .color = colorWhite,
             .height = fontHeightWithALittlePadding
     });
-    lines.push_back(s_CreditLine {
+    m_lines.push_back(s_CreditLine {
             .txt = "Bruno - For your lifelong support & valuable feedback",
             .color = colorWhite,
             .height = fontHeightWithALittlePadding
     });
 
-    lines.push_back(spacer);
+    m_lines.push_back(spacer);
 
-    lines.push_back(s_CreditLine {
+    m_lines.push_back(s_CreditLine {
             .txt = "--- ACKNOWLEDGEMENTS ---",
             .color = colorHeadlines,
             .height = spacerAfterHeadline
     });
-    lines.push_back(s_CreditLine {
+    m_lines.push_back(s_CreditLine {
             .txt = "Westwood Studios - for producing Dune 2",
             .color = colorWhite,
             .height = fontHeightWithALittlePadding
     });
-    lines.push_back(s_CreditLine {
+    m_lines.push_back(s_CreditLine {
             .txt = "Allegro - by Shawn Hargreaves (et al)",
             .color = colorWhite,
             .height = fontHeightWithALittlePadding
     });
-    lines.push_back(s_CreditLine {
+    m_lines.push_back(s_CreditLine {
             .txt = "AlFONT - by Javier Gonzalez",
             .color = colorWhite,
             .height = fontHeightWithALittlePadding
     });
-    lines.push_back(s_CreditLine {
+    m_lines.push_back(s_CreditLine {
             .txt = "Dune Legacy, Dune Dynasty and Dune The Golden Path",
             .color = colorWhite,
             .height = fontHeightWithALittlePadding
     });
 
-    lines.push_back(spacer);
+    m_lines.push_back(spacer);
 
-    lines.push_back(s_CreditLine {
+    m_lines.push_back(s_CreditLine {
             .txt = "--- KO-FI SUPPORTERS ---",
             .color = colorHeadlines,
             .height = spacerAfterHeadline
     });
-    lines.push_back(s_CreditLine {
+    m_lines.push_back(s_CreditLine {
             .txt = "Fedaygin",
             .color = colorWhite,
             .height = fontHeightWithALittlePadding
     });
-    lines.push_back(s_CreditLine {
+    m_lines.push_back(s_CreditLine {
             .txt = "Morto",
             .color = colorWhite,
             .height = fontHeightWithALittlePadding
     });
-    lines.push_back(s_CreditLine {
+    m_lines.push_back(s_CreditLine {
             .txt = "The General",
             .color = colorWhite,
             .height = spacerAfterHeadline
     });
-    lines.push_back(spacer);
+    m_lines.push_back(spacer);
 
-    lines.push_back(s_CreditLine {
+    m_lines.push_back(s_CreditLine {
             .txt = "--- SPECIAL THANKS TO ---",
             .color = colorHeadlines,
             .height = spacerAfterHeadline
     });
-    lines.push_back(s_CreditLine {
+    m_lines.push_back(s_CreditLine {
             .txt = "Gobalopper - for Dune2K, and hosting arrakis.dune2k.com",
             .color = colorWhite,
             .height = fontHeightWithALittlePadding
     });
-    lines.push_back(s_CreditLine {
+    m_lines.push_back(s_CreditLine {
             .txt = "Jpexs - for help with the Dune 2 Seed Generator",
             .color = colorWhite,
             .height = fontHeightWithALittlePadding
     });
-    lines.push_back(s_CreditLine {
+    m_lines.push_back(s_CreditLine {
             .txt = "Rene - for the D2TM Java Map Editor",
             .color = colorWhite,
             .height = fontHeightWithALittlePadding
     });
-    lines.push_back(s_CreditLine {
+    m_lines.push_back(s_CreditLine {
             .txt = "Guillaume Lavoie",
             .color = colorWhite,
             .height = fontHeightWithALittlePadding
     });
-    lines.push_back(s_CreditLine {
+    m_lines.push_back(s_CreditLine {
             .txt = "Dennis Stansert",
             .color = colorWhite,
             .height = fontHeightWithALittlePadding
     });
-    lines.push_back(s_CreditLine {
+    m_lines.push_back(s_CreditLine {
             .txt = "Ash",
             .color = colorWhite,
             .height = fontHeightWithALittlePadding
     });
-    lines.push_back(s_CreditLine {
+    m_lines.push_back(s_CreditLine {
             .txt = "Ruurd Nijdam",
             .color = colorWhite,
             .height = fontHeightWithALittlePadding
     });
-    lines.push_back(s_CreditLine {
+    m_lines.push_back(s_CreditLine {
             .txt = "rphl",
             .color = colorWhite,
             .height = fontHeightWithALittlePadding
     });
-    lines.push_back(s_CreditLine {
-            .txt = "All those at the dune2k arrakis forums, for the good times",
+    m_lines.push_back(s_CreditLine {
+            .txt = "Non sono Mandrake - for nudging me to work on D2TM again in C++",
+            .color = colorWhite,
+            .height = fontHeightWithALittlePadding
+    });
+    m_lines.push_back(s_CreditLine {
+            .txt = "All those at the dune2k arrakis forums and the good times",
             .color = colorWhite,
             .height = fontHeightWithALittlePadding
     });
 
     // spacer
     // and finally a thank you for the player
-    lines.push_back(spacer);
+    m_lines.push_back(spacer);
 
-    lines.push_back(s_CreditLine {
-            .txt = "Thank you for playing Dune 2 - The Maker",
-            .color = colorHeadlines,
+    m_lines.push_back(s_CreditLine {
+            .txt = "Thank you for playing",
+            .color = colorWhite,
             .height = spacerAfterHeadline
     });
 }
 
 void cCreditsState::resetCrawler() {
-    crawlerY = game.m_screenY + 1;
+    m_crawlerY = game.m_screenY + 1;
 }
 
 cCreditsState::~cCreditsState() {
-    bmp_Dune = nullptr;
-    bmp_D2TM_Title = nullptr;
+    m_duneBmp = nullptr;
+    m_titleBmp = nullptr;
 }
 
 void cCreditsState::thinkFast() {
-    crawlerY -= moveSpeed;
+    m_crawlerY -= m_moveSpeed;
 
-    int textCrawlY = crawlerY + logoHeight;
-    for (auto & line : lines) {
+    int textCrawlY = m_crawlerY + m_titleHeight;
+    for (auto & line : m_lines) {
         textCrawlY += line.height;
     }
 
@@ -256,13 +262,13 @@ void cCreditsState::thinkFast() {
 void cCreditsState::draw() const {
     clear_to_color(bmp_screen, makecol(0, 0, 16));
 
-    draw_sprite(bmp_screen, bmp_Dune, coords_Dune.x, coords_Dune.y);
+    draw_sprite(bmp_screen, m_duneBmp, m_duneCoordinates.x, m_duneCoordinates.y);
 
     // draw crawler
-    draw_sprite(bmp_screen, bmp_D2TM_Title, logoX, crawlerY);
-    int textCrawlY = crawlerY + logoHeight;
-    for (auto & line : lines) {
-        textDrawer.drawTextCentered(line.txt.c_str(), textCrawlY, line.color);
+    draw_sprite(bmp_screen, m_titleBmp, m_titleX, m_crawlerY);
+    int textCrawlY = m_crawlerY + m_titleHeight;
+    for (auto & line : m_lines) {
+        m_textDrawer.drawTextCentered(line.txt.c_str(), textCrawlY, line.color);
         textCrawlY += line.height;
     }
 
@@ -272,9 +278,9 @@ void cCreditsState::draw() const {
 
 void cCreditsState::onNotifyMouseEvent(const s_MouseEvent &event) {
     if (event.eventType == eMouseEventType::MOUSE_LEFT_BUTTON_PRESSED) {
-        moveSpeed = 1.0f; // speed up
+        m_moveSpeed = 1.0f; // speed up
     } else if (event.eventType == eMouseEventType::MOUSE_LEFT_BUTTON_CLICKED) {
-        moveSpeed = 0.15f; // restore
+        m_moveSpeed = 0.15f; // restore
     }
 }
 
