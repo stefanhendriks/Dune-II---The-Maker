c47558b0875bdee28bfdc46d4dc84d038784d256
Stefan Hendriks <stefan@kaizenbv.nl>
2022-02-26 11:34:00 +0100

    Few cosmetic things
    - naming, so that members have m_
    - reformat with my IDE
    - print out argument that is invalid

--- utils/cHandleArgument.cpp
+++ utils/cHandleArgument.cpp
@@ -1,98 +1,120 @@
-#include "cHandleArgument.hpp"
+#include "cHandleArgument.h"
 #include "cGame.h"
 #include <iostream>
 
-cHandleArgument::cHandleArgument(cGame *_game){
-    game=_game;
+cHandleArgument::cHandleArgument(cGame *game) :
+m_game(game),
+m_optionToHandleAfter(),
+m_argumentScreenX(800),
+m_argumentScreenY(600)
+{
+
 }
 
-cHandleArgument::~cHandleArgument()
-{}
+cHandleArgument::~cHandleArgument() {}
 
+/**
+ * Parses arguments given. Returns 0 on success or < 0 on failure.
+ *
+ * @param argc
+ * @param argv
+ * @return
+ */
 int cHandleArgument::handleArguments(int argc, char *argv[]) {
+    // TODO: make this return a config object, and not be tightly coupled to cGame
+    m_game->m_disableAI = false;
+    m_game->m_disableReinforcements = false;
+    m_game->m_drawUsages = false;
+    m_game->m_drawUnitDebug = false;
+    m_game->m_oneAi = false;
+    m_game->m_windowed = false;
+    m_game->m_noAiRest = false;
+    m_game->setDebugMode(false);
 
-    game->m_disableAI = false;
-    game->m_disableReinforcements = false;
-    game->m_drawUsages = false;
-    game->m_drawUnitDebug = false;
-    game->m_oneAi = false;
-    game->m_windowed = false;
-    game->m_noAiRest = false;
-    game->setDebugMode(false);
-
-	if (argc <2) {
+    if (argc < 2) {
         return 0;
     }
 
     for (int i = 1; i < argc; i++) {
         std::string command = argv[i];
         auto itr = optionStrings.find(command);
-        if( itr == optionStrings.end() ) {
-            std::cerr << "Unknown option, check with -usages" << std::endl;
+        if (itr == optionStrings.end()) {
+            std::cerr << "Unknown option \"" << command << "\", check with -usages" << std::endl;
             return -1;
         }
 
         switch (itr->second) {
-            case    Options::GAME :  if ((i + 1) < argc) {
-					    i++;
-					    game->setGameFilename(std::string(argv[i]));
-                        } break;
-            case    Options::WINDOWED:  // Windowed flag passed, so use that
-                        optionToHandleAfter[Options::WINDOWED] = true;
-                        break;
-            case    Options::NOMUSIC:
-                        game->m_playMusic = false;
-                        break;
-            case    Options::NOSOUND:   // disable all sound effects
-			        	game->m_playMusic = false;
-			        	game->m_playSound = false;
-                        break;
-            case    Options::DEBUG:   // generic debugging enabled
-			        	game->setDebugMode(true);
-                        break;
-            case    Options::DEBUG_UNITS:   // unit debugging enabled
-			        	game->m_drawUnitDebug = true;
-                        break;
-            case    Options::NOAI:
-			        	game->m_disableAI = true;
-                        break;
-            case    Options::ONEAI:
-			        	game->m_oneAi = true;
-                        break;
-            case    Options::NOREINFORCEMENTS:
-			        	game->m_disableReinforcements = true;
-                        break;
-            case    Options::NOAIREST:
-			        	game->m_noAiRest = true;
-                        break;
-            case    Options::SCREENX:
-                        if ((i + 1) < argc) {
-					        i++;
-					        optionToHandleAfter[Options::SCREENX] = true;
-                            ArgumentScreenX = atoi(argv[i]);
-                        } break;
-            case    Options::SCREENY:
-                        if ((i + 1) < argc) {
-					        i++;
-					        optionToHandleAfter[Options::SCREENY] = true;
-                            ArgumentScreenY = atoi(argv[i]);
-                        } break;
-            case    Options::USAGES:
-			default : 
-                    game->m_drawUsages = true;
-                    break;
+            case Options::GAME :
+                if ((i + 1) < argc) {
+                    i++;
+                    m_game->setGameFilename(std::string(argv[i]));
+                }
+                break;
+            case Options::WINDOWED:  // Windowed flag passed, so use that
+                m_optionToHandleAfter[Options::WINDOWED] = true;
+                break;
+            case Options::NOMUSIC:
+                m_game->m_playMusic = false;
+                break;
+            case Options::NOSOUND:   // disable all sound effects
+                m_game->m_playMusic = false;
+                m_game->m_playSound = false;
+                break;
+            case Options::DEBUG:   // generic debugging enabled
+                m_game->setDebugMode(true);
+                break;
+            case Options::DEBUG_UNITS:   // unit debugging enabled
+                m_game->m_drawUnitDebug = true;
+                break;
+            case Options::NOAI:
+                m_game->m_disableAI = true;
+                break;
+            case Options::ONEAI:
+                m_game->m_oneAi = true;
+                break;
+            case Options::NOREINFORCEMENTS:
+                m_game->m_disableReinforcements = true;
+                break;
+            case Options::NOAIREST:
+                m_game->m_noAiRest = true;
+                break;
+            case Options::SCREENX:
+                if ((i + 1) < argc) {
+                    i++;
+                    m_optionToHandleAfter[Options::SCREENX] = true;
+                    m_argumentScreenX = atoi(argv[i]);
+                }
+                break;
+            case Options::SCREENY:
+                if ((i + 1) < argc) {
+                    i++;
+                    m_optionToHandleAfter[Options::SCREENY] = true;
+                    m_argumentScreenY = atoi(argv[i]);
+                }
+                break;
+            case Options::USAGES:
+            default :
+                m_game->m_drawUsages = true;
+                break;
         }
-	} // arguments passed
-	return 0;
+    } // arguments passed
+    return 0;
 }
 
-void cHandleArgument::applyArguments(){
-    for (const auto& [key, value] : optionToHandleAfter) {
+void cHandleArgument::applyArguments() {
+    for (const auto&[key, value]: m_optionToHandleAfter) {
         switch (key) {
-            case    Options::WINDOWED:  game->m_windowed = value;   break;
-            case    Options::SCREENX:   game->m_screenX = ArgumentScreenX; break;
-            case    Options::SCREENY:   game->m_screenY = ArgumentScreenY; break;
-            default:    break;
+            case Options::WINDOWED:
+                m_game->m_windowed = value;
+                break;
+            case Options::SCREENX:
+                m_game->m_screenX = m_argumentScreenX;
+                break;
+            case Options::SCREENY:
+                m_game->m_screenY = m_argumentScreenY;
+                break;
+            default:
+                break;
         }
     }
 }
---
+++ utils/cHandleArgument.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include <map>
+#include <string>
+
+class cGame;
+
+class cHandleArgument {
+
+public:
+    explicit cHandleArgument(cGame * game);
+
+    ~cHandleArgument();
+
+    int handleArguments(int argc, char *argv[]);
+
+    void applyArguments();
+
+private:
+    enum class Options : char {
+        GAME,
+        WINDOWED,
+        NOMUSIC,
+        NOSOUND,
+        DEBUG,
+        DEBUG_UNITS,
+        NOAI,
+        ONEAI,
+        NOREINFORCEMENTS,
+        NOAIREST,
+        USAGES,
+        SCREENX,
+        SCREENY
+    };
+
+    const std::map<std::string, Options> optionStrings{
+            {"-game",             Options::GAME},
+            {"-windowed",         Options::WINDOWED},
+            {"-nomusic",          Options::NOMUSIC},
+            {"-nosound",          Options::NOSOUND},
+            {"-debug",            Options::DEBUG},
+            {"-debug-units",      Options::DEBUG_UNITS},
+            {"-noai",             Options::NOAI},
+            {"-oneai",            Options::ONEAI},
+            {"-noreinforcements", Options::NOREINFORCEMENTS},
+            {"-noairest",         Options::NOAIREST},
+            {"-screenWidth",      Options::SCREENX},
+            {"-screenHeight",     Options::SCREENY},
+            {"-usages",           Options::USAGES}
+    };
+
+    cGame *m_game = nullptr;
+
+    std::map<Options, bool> m_optionToHandleAfter;
+
+    int m_argumentScreenX, m_argumentScreenY;
+};
