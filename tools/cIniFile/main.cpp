9c47271cc4fc502c548d498c92ac1996aa0a4e8d
Mira57 <olivier.nivoix@gmail.com>
2022-03-01 19:13:47 +0100

    cIniFile test

---
+++ tools/cIniFile/main.cpp
@@ -0,0 +1,24 @@
+#include "cIniFile.h"
+#include <vector>
+#include <iostream>
+
+using namespace std;
+  
+int main()  
+{  
+    cIniFile cini("./test.ini");  
+  
+    std::string str1 = cini.getStr("system","param1");
+    std::string str2 = cini.getStr("system","param2");
+    std::string str3 = cini.getStr("system","param3");
+    
+    std::string str4 = cini.getStr("config","param3");
+
+    std::cout << "param1 " << str1 << std::endl;
+    std::cout << "param2 " << str2 << std::endl;
+    std::cout << "param3 " << str3 << std::endl;
+
+    std::cout << "param3 " << str4 << std::endl;
+
+    return 0;  
+}  
