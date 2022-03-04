aed330135e2f7578417f22b0945b5a4194945c2b
Mira57 <olivier.nivoix@gmail.com>
2022-03-04 06:57:35 +0100

    Implementation cPreviewMaps

--- map/cPreviewMaps.h
+++ map/cPreviewMaps.h
@@ -1,11 +1,30 @@
 #pragma once
 
+#define MAX_SKIRMISHMAPS 100     // max of 100 skirmish maps
+
+#include <allegro.h>
+#include <vector>
+#include <string>
+
+struct s_PreviewMap {
+     BITMAP *terrain;        // terrain
+     int width, height;      // width & height of map
+     std::vector<int> mapdata;
+     int iPlayers;           // players
+     int iStartCell[5];      // starting locations
+     std::string name;       // name
+};
+
 class cPreviewMaps
 {
 public:
     cPreviewMaps();
     ~cPreviewMaps();
     void prescanSkirmish();
+    void INI_PRESCAN_SKIRMISH();
+    s_PreviewMap& getMap(int i)  { if (i > MAX_SKIRMISHMAPS) return PreviewMap[0]; return PreviewMap[i]; }
+    void INI_LOAD_SKIRMISH(const char filename[80]);
+    void INIT_PREVIEWS();
 private:
-    
+    s_PreviewMap PreviewMap[MAX_SKIRMISHMAPS];
 };
