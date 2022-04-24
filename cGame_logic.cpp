4ba7cab30489bc2df730aac620fb20a219d54cd0
Stefan Hendriks <stefan@fundynamic.com>
2022-04-24 21:47:10 +0200

    Mark as 0.7.0 Release Candidate

--- cGame_logic.cpp
+++ cGame_logic.cpp
@@ -86,7 +86,7 @@ cGame::cGame() : m_timeManager(*this) {
     m_iniScreenWidth = -1;
     m_iniScreenHeight = -1;
 
-    m_version = "0.6.x";
+    m_version = "0.7.0-RC";
 
     m_mentat = nullptr;
     m_handleArgument = std::make_unique<cHandleArgument>(this);
