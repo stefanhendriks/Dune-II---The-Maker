28898bf16357f1d0fe2f6a25010a380d6808a424
Stefan Hendriks <stefan@fundynamic.com>
2022-06-01 21:23:53 +0200

    Make it compile against the new cIni / cSection classes interfaces

--- player/cHousesInfo.cpp
+++ player/cHousesInfo.cpp
@@ -2,37 +2,39 @@
 #include "include/definitions.h"
 #include "utils/common.h"
 
-void cHousesInfo::INSTALL_HOUSES(std::shared_ptr<cIniFile> gamesCfg) {
+void cHousesInfo::INSTALL_HOUSES(std::shared_ptr<cIniFile> gameCfg) {
+    const cSection &section = gameCfg->getSection(SECTION_HOUSES);
+
     // General / Default / No House
-    sHouseInfo[GENERALHOUSE].swap_color = gamesCfg->getInt("HOUSES","GENERAL_SWAPCOLOR");
-    sHouseInfo[GENERALHOUSE].minimap_color = makeColFromString(gamesCfg->getStr("HOUSES","GENERAL_MINIMAPCOLOR"));
+    sHouseInfo[GENERALHOUSE].swap_color = section.getInt("GENERAL_SWAPCOLOR");
+    sHouseInfo[GENERALHOUSE].minimap_color = makeColFromString(section.getStringValue("GENERAL_MINIMAPCOLOR"));
 
     // Harkonnen
-    sHouseInfo[HARKONNEN].swap_color = gamesCfg->getInt("HOUSES","HARKONNEN_SWAPCOLOR");
-    sHouseInfo[HARKONNEN].minimap_color = makeColFromString(gamesCfg->getStr("HOUSES","HARKONNEN_MINIMAPCOLOR"));
+    sHouseInfo[HARKONNEN].swap_color = section.getInt("HARKONNEN_SWAPCOLOR");
+    sHouseInfo[HARKONNEN].minimap_color = makeColFromString(section.getStringValue("HARKONNEN_MINIMAPCOLOR"));
 
     // Atreides
-    sHouseInfo[ATREIDES].swap_color = gamesCfg->getInt("HOUSES","ATREIDES_SWAPCOLOR");
-    sHouseInfo[ATREIDES].minimap_color = makeColFromString(gamesCfg->getStr("HOUSES","ATREIDES_MINIMAPCOLOR"));
+    sHouseInfo[ATREIDES].swap_color = section.getInt("ATREIDES_SWAPCOLOR");
+    sHouseInfo[ATREIDES].minimap_color = makeColFromString(section.getStringValue("ATREIDES_MINIMAPCOLOR"));
 
     // Ordos
-    sHouseInfo[ORDOS].swap_color = gamesCfg->getInt("HOUSES","ORDOS_SWAPCOLOR");
-    sHouseInfo[ORDOS].minimap_color = makeColFromString(gamesCfg->getStr("HOUSES","ORDOS_MINIMAPCOLOR"));
+    sHouseInfo[ORDOS].swap_color = section.getInt("ORDOS_SWAPCOLOR");
+    sHouseInfo[ORDOS].minimap_color = makeColFromString(section.getStringValue("ORDOS_MINIMAPCOLOR"));
 
     // Mercenary
-    sHouseInfo[MERCENARY].swap_color = gamesCfg->getInt("HOUSES","MERCENARY_SWAPCOLOR");
-    sHouseInfo[MERCENARY].minimap_color = makeColFromString(gamesCfg->getStr("HOUSES","MERCENARY_MINIMAPCOLOR"));
+    sHouseInfo[MERCENARY].swap_color = section.getInt("MERCENARY_SWAPCOLOR");
+    sHouseInfo[MERCENARY].minimap_color = makeColFromString(section.getStringValue("MERCENARY_MINIMAPCOLOR"));
 
     // Sardaukar
-    sHouseInfo[SARDAUKAR].swap_color = gamesCfg->getInt("HOUSES","SARDAUKAR_SWAPCOLOR");
-    sHouseInfo[SARDAUKAR].minimap_color = makeColFromString(gamesCfg->getStr("HOUSES","SARDAUKAR_MINIMAPCOLOR"));
+    sHouseInfo[SARDAUKAR].swap_color = section.getInt("SARDAUKAR_SWAPCOLOR");
+    sHouseInfo[SARDAUKAR].minimap_color = makeColFromString(section.getStringValue("SARDAUKAR_MINIMAPCOLOR"));
 
     // Fremen
-    sHouseInfo[FREMEN].swap_color = gamesCfg->getInt("HOUSES","FREMEN_SWAPCOLOR");
-    sHouseInfo[FREMEN].minimap_color = makeColFromString(gamesCfg->getStr("HOUSES","FREMEN_MINIMAPCOLOR"));
+    sHouseInfo[FREMEN].swap_color = section.getInt("FREMEN_SWAPCOLOR");
+    sHouseInfo[FREMEN].minimap_color = makeColFromString(section.getStringValue("FREMEN_MINIMAPCOLOR"));
 
     // GREY
     // ???
-    sHouseInfo[CORRINO].swap_color = gamesCfg->getInt("HOUSES","CORRINO_SWAPCOLOR");
-    sHouseInfo[CORRINO].minimap_color = makeColFromString(gamesCfg->getStr("HOUSES","CORRINO_MINIMAPCOLOR"));
+    sHouseInfo[CORRINO].swap_color = section.getInt("CORRINO_SWAPCOLOR");
+    sHouseInfo[CORRINO].minimap_color = makeColFromString(section.getStringValue("CORRINO_MINIMAPCOLOR"));
 }
