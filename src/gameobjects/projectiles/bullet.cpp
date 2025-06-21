3ed0382ff40ba2948830ca2b3effe186e8f0d7f5
Olivier NIVOIX <olivier.nivoix@gmail.com>
2025-06-21 09:48:38 +0200

    Fix wrong constructor II

--- src/gameobjects/projectiles/bullet.cpp
+++ src/gameobjects/projectiles/bullet.cpp
@@ -122,7 +122,7 @@ void cBullet::draw()
 
     // Whenever this bullet is a shimmer, draw a shimmer and leave
     if (iType == BULLET_SHIMMER) {
-        renderDrawer->shimmer(mapCamera->factorZoomLevel(16), x, y, mapCamera->factorZoomLevel(4));
+        renderDrawer->shimmer(bmp_screen, mapCamera->factorZoomLevel(16), x, y, mapCamera->factorZoomLevel(4));
         return;
     }
 
