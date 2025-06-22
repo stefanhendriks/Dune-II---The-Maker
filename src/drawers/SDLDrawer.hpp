6dfcd51ac448129bd810c5ee4432d5fe92b88d2a
Mira <mira@legendes-oubliees.org>
2025-06-22 09:11:48 +0200

    drawDot without dest

--- src/drawers/SDLDrawer.hpp
+++ src/drawers/SDLDrawer.hpp
@@ -65,7 +65,7 @@ public:
     void gui_DrawRectBorder(const cRectangle &rectangle, SDL_Color gui_colorBorderLight, SDL_Color gui_colorBorderDark);
 
     void drawLine(int x1, int y1, int x2, int y2, SDL_Color color);
-    void drawDot(SDL_Surface *bmp, int x, int y, SDL_Color color, int size);
+    void drawDot(int x, int y, SDL_Color color, int size);
 
     void shimmer(SDL_Surface *src, int r, int x, int y, float cameraZoom);
 
