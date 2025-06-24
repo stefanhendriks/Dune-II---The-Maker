1d6a48c17a0b5da3e804d00f1b2c7b2e4ef96c6d
Mira <mira@legendes-oubliees.org>
2025-06-24 19:57:33 +0200

    Now use renderSprite

--- src/drawers/SDLDrawer.hpp
+++ src/drawers/SDLDrawer.hpp
@@ -18,9 +18,10 @@ public:
     void drawCenteredSprite(SDL_Surface *dest, SDL_Surface *src);
     void drawTransSprite(SDL_Surface *sprite, SDL_Surface *dest, int x, int y);
 
-    void drawSprite(SDL_Surface *src, int x, int y,unsigned char opacity = 255);
-    void drawSprite(SDL_Surface *dest, SDL_Surface *src, int x, int y,unsigned char opacity = 255);
-    void drawSprite(SDL_Surface *dest, int index, int x, int y, unsigned char opacity = 255);
+    void renderSprite(SDL_Texture *src,int x, int y,unsigned char opacity = 255);
+    [[deprecated]] void drawSprite(SDL_Surface *src, int x, int y,unsigned char opacity = 255);
+    [[deprecated]] void drawSprite(SDL_Surface *dest, SDL_Surface *src, int x, int y,unsigned char opacity = 255);
+    [[deprecated]] void drawSprite(SDL_Surface *dest, int index, int x, int y, unsigned char opacity = 255);
 
     void drawSimpleColor(int x, int y, int width, int height, Uint8 r, Uint8 g, Uint8 b, unsigned char opacyty);
 
