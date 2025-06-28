c198065a641e98eda21f3c867cf77943b34a520b
Olivier NIVOIX <olivier.nivoix@gmail.com>
2025-06-28 05:58:06 +0200

    Create renderFromSurface

--- src/drawers/SDLDrawer.hpp
+++ src/drawers/SDLDrawer.hpp
@@ -25,6 +25,7 @@ public:
     [[deprecated]] void drawSprite(SDL_Surface *src, int x, int y,unsigned char opacity = 255);
     [[deprecated]] void drawSprite(SDL_Surface *dest, SDL_Surface *src, int x, int y,unsigned char opacity = 255);
     [[deprecated]] void drawSprite(SDL_Surface *dest, int index, int x, int y, unsigned char opacity = 255);
+    void renderFromSurface(SDL_Surface *src, int x, int y,unsigned char opacity = 255);
 
     void renderRectFillColor(int x, int y, int width, int height, Uint8 r, Uint8 g, Uint8 b, unsigned char opacyty=255);
     void renderRectFillColor(int x, int y, int width, int height, SDL_Color color);
