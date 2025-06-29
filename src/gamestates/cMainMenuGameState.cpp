dd0e0e772daa60a20e0b54744a81987fbd3ec128
Mira <mira@legendes-oubliees.org>
2025-06-29 06:28:43 +0200

    Move localisation

--- src/gamestates/cMainMenuGameState.cpp
+++ src/gamestates/cMainMenuGameState.cpp
@@ -50,7 +50,7 @@ cMainMenuGameState::cMainMenuGameState(cGame &theGame) : cGameState(theGame), te
     int buttonHeight = textDrawer.getFontHeight() + 4; // a bit more space
 
     int creditsX = (game.m_screenW / 2) - buttonWidth;
-    GUI_DRAW_BENE_TEXT_MOUSE_SENSITIVE(creditsX, 1, "CREDITS", SDL_Color{64, 64, 64,255});
+    //GUI_DRAW_BENE_TEXT_MOUSE_SENSITIVE(creditsX, 1, "CREDITS", SDL_Color{64, 64, 64,255});
     const cRectangle &creditsRect = cRectangle(creditsX, 0, buttonWidth, buttonHeight);
     gui_btn_credits = new cGuiButton(textDrawer, creditsRect, "CREDITS", buttonKinds);
     gui_btn_credits->setTextAlignHorizontal(buttonTextAlignment);
