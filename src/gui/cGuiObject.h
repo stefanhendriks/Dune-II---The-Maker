1f51dd355d9599da5e8985fe12d3b2e8d39c2308
Mira <olivier.nivoix@gmail.com>
2025-07-04 17:57:56 +0200

    GuiTheme integred in GuiObject

--- src/gui/cGuiObject.h
+++ src/gui/cGuiObject.h
@@ -2,15 +2,17 @@
 
 #include "observers/cInputObserver.h"
 #include "utils/cRectangle.h"
+#include "gui/GuiTheme.hpp"
 
 class cGuiObject : public cInputObserver {
 public:
     virtual ~cGuiObject() = default;
-
     virtual void draw() const = 0;
 
+    void setTheme(const GuiTheme& theme);
 protected:
     explicit cGuiObject(const cRectangle &rect) : m_rect(rect) {}
     // any gui object has a position and size. Hence its always a 'rect'.
     cRectangle m_rect;
+    GuiTheme m_theme;
 };
