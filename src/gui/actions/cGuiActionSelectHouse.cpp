79c79cb78648e24beb69382ee2ad5c036de8d5a7
Mira <olivier.nivoix@gmail.com>
2025-07-05 05:25:29 +0200

    remove GuiAction SelectHouse

--- src/gui/actions/cGuiActionSelectHouse.cpp
+++
@@ -1,9 +0,0 @@
-#include "cGuiActionSelectHouse.h"
-
-#include "d2tmc.h"
-
-void cGuiActionSelectHouse::execute()
-{
-    game.setNextStateToTransitionTo(GAME_SELECT_HOUSE);
-    game.initiateFadingOut();
-}
