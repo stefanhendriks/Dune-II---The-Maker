f7c7e2b1b0db1f32bb3790e718023391f82c8ca0
Mira <mira@legendes-oubliees.org>
2025-07-09 07:25:23 +0200

    Create ContextCreator

---
+++ src/context/ContextCreator.cpp
@@ -0,0 +1,22 @@
+#include "context/ContextCreator.hpp"
+#include "utils/Graphics.hpp"
+#include "utils/cLog.h"
+#include "utils/cFileValidator.h"
+#include "context/EntityContext.hpp"
+
+ContextCreator::ContextCreator(SDL_Renderer *renderer, cFileValidator *settingsValidator)
+{
+    gfxmentat = std::make_shared<Graphics>(renderer,settingsValidator->getFullName(eGameDirFileName::GFXMENTAT));
+    if (gfxmentat == nullptr) {
+        cLogger::getInstance()->log(LOG_ERROR, COMP_ALLEGRO, "Load data", "Could not hook/load datafile:" + settingsValidator->getName(eGameDirFileName::GFXMENTAT), OUTC_FAILED);
+    }
+    else {
+        cLogger::getInstance()->log(LOG_INFO, COMP_ALLEGRO, "Load data", "Hooked datafile: " + settingsValidator->getName(eGameDirFileName::GFXMENTAT), OUTC_SUCCESS);
+    }
+}
+
+std::unique_ptr<GraphicsContext> ContextCreator::createGraphicsContext()
+{
+    auto gtx = std::make_unique<GraphicsContext>();
+    gtx->gfxmentat = gfxmentat;
+}
