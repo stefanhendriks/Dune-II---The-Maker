a3b562ed98cec4c9255f8d128734725047de38bd
Mira <mira@legendes-oubliees.org>
2025-07-10 22:20:43 +0200

    Integration Ctx  on cSelectYourNextRegionSceneState

--- src/context/ContextCreator.cpp
+++ src/context/ContextCreator.cpp
@@ -16,10 +16,10 @@ ContextCreator::ContextCreator(SDL_Renderer *renderer, cFileValidator *settingsV
 
     gfxworld = std::make_shared<Graphics>(renderer,settingsValidator->getFullName(eGameDirFileName::GFXWORLD));
     if (gfxworld == nullptr) {
-        cLogger::getInstance()->log(LOG_ERROR, COMP_ALLEGRO, "Load data", "Could not hook/load datafile:" + settingsValidator->getName(eGameDirFileName::GFXWORLD), OUTC_FAILED);
+        cLogger::getInstance()->log(LOG_ERROR, COMP_INIT, "Load data", "Could not hook/load datafile:" + settingsValidator->getName(eGameDirFileName::GFXWORLD), OUTC_FAILED);
     }
     else {
-        cLogger::getInstance()->log(LOG_INFO, COMP_ALLEGRO, "Load data", "Hooked datafile: " + settingsValidator->getName(eGameDirFileName::GFXWORLD), OUTC_SUCCESS);
+        cLogger::getInstance()->log(LOG_INFO, COMP_INIT, "Load data", "Hooked datafile: " + settingsValidator->getName(eGameDirFileName::GFXWORLD), OUTC_SUCCESS);
     }
 }
 
