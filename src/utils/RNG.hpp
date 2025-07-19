6c62b9b14d20505ba6289c73e96aab24c6f3aa84
Mira <olivier.nivoix@gmail.com>
2025-07-19 06:39:23 +0200

    Documentation

--- src/utils/RNG.hpp
+++ src/utils/RNG.hpp
@@ -1,16 +1,18 @@
 #pragma once
 #include <random>
 
+// Class that centralizes all requests for random numbers
+
 class RNG {
 public:
+    //initialization of the number generator
     static std::mt19937& getGenerator();
-
+    //Returns an integer between min (included) and max (included)
     static int genInt(int min, int max);
-
+    //Returns an integer between 0 (included) and max(included)
     static int rnd(int max);
-
+    // Returns a decimal number between min (included) and max (included)
     static double genDouble(double min, double max);
-
 private:
     RNG() = default;
     RNG(const RNG&) = delete;
