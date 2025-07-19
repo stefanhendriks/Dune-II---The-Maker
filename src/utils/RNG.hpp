e0e4fbf10a52fe75734405f243b10baa082e02f8
Mira <olivier.nivoix@gmail.com>
2025-07-19 06:36:02 +0200

    Move member function definitions

--- src/utils/RNG.hpp
+++ src/utils/RNG.hpp
@@ -3,28 +3,13 @@
 
 class RNG {
 public:
-    static std::mt19937& getGenerator() {
-        static std::random_device rd;
-        static std::mt19937 s_generator(rd());
-        return s_generator;
-    }
+    static std::mt19937& getGenerator();
 
-    static int genInt(int min, int max) {
-        std::uniform_int_distribution<int> dist(min, max);
-        return dist(getGenerator());
-    }
+    static int genInt(int min, int max);
 
-    static int rnd(int max) {
-        if (max<1)
-            return 0;
-        std::uniform_int_distribution<int> dist(0, max);
-        return dist(getGenerator());
-    }
+    static int rnd(int max);
 
-    static double genDouble(double min, double max) {
-        std::uniform_real_distribution<double> dist(min, max);
-        return dist(getGenerator());
-    }
+    static double genDouble(double min, double max);
 
 private:
     RNG() = default;
