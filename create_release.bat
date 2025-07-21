810e730827b26e65febe319aa0b4c5b7ed46c677
Stefan Hendriks <shendriks@keesdeboekhouder.nl>
2025-07-21 18:14:12 +0200

    Move resources required to build binary & docs into resources folder
    - update the create_release* files so they point to the new locations
    - introduce CODEOWNERS

--- create_release.bat
+++ create_release.bat
@@ -19,24 +19,24 @@ mkdir bin\campaign\maps
 mkdir bin\campaign\maps\seed
 mkdir bin\campaign\briefings
 echo 4. Copying campaign files
-copy campaign\atreides\*.* bin\campaign\atreides
-copy campaign\ordos\*.* bin\campaign\ordos
-copy campaign\harkonnen\*.* bin\campaign\harkonnen
-copy campaign\maps\*.* bin\campaign\maps
-copy campaign\maps\seed\*.* bin\campaign\maps\seed
-copy campaign\briefings\*.* bin\campaign\briefings
-copy campaign\info.txt bin\campaign\info.txt
+copy resources\bin\campaign\atreides\*.* bin\campaign\atreides
+copy resources\bin\campaign\ordos\*.* bin\campaign\ordos
+copy resources\bin\campaign\harkonnen\*.* bin\campaign\harkonnen
+copy resources\bin\campaign\maps\*.* bin\campaign\maps
+copy resources\bin\campaign\maps\seed\*.* bin\campaign\maps\seed
+copy resources\bin\campaign\briefings\*.* bin\campaign\briefings
+copy resources\bin\campaign\info.txt bin\campaign\info.txt
 echo 5. Creating new data directory
 mkdir bin\data
 mkdir bin\data\scenes
 mkdir bin\data\bmp
 echo 6. Copying data files
-copy data\*.dat bin\data
-copy data\*.fon bin\data
-copy data\*.ttf bin\data
-copy data\bmp\*.* bin\data\bmp
+copy resources\bin\data\*.dat bin\data
+copy resources\bin\data\*.fon bin\data
+copy resources\bin\data\*.ttf bin\data
+copy resources\bin\data\bmp\*.* bin\data\bmp
 echo 7. Copying scenes
-copy data\scenes\*.* bin\data\scenes
+copy resources\bin\data\scenes\*.* bin\data\scenes
 echo 8. Create save game directories
 mkdir bin\save
 mkdir bin\save\atreides
@@ -45,18 +45,17 @@ mkdir bin\save\ordos
 mkdir bin\save\skirmish
 echo 9. Create skirmish directory
 mkdir bin\skirmish
-copy skirmish\*.ini bin\skirmish
+copy resources\bin\skirmish\*.ini bin\skirmish
 echo 10. Copying dll files
-copy dll\mingw32\*.* bin
+copy resources\bin\dll\*.* bin
 echo 11. Copy executable from %1
 copy cmake-build-release-mingw\*.exe bin
 echo 12. Copy game rules file (game.ini)
-copy game.ini.org bin\game.ini
+copy resources\game.ini.org bin\game.ini
 echo 13. Copy txt files
 copy *.txt bin
-copy doc\versionhistory.txt bin
-copy doc\controls.txt bin
-copy windowed.bat bin
-copy d2tm.cfg bin
-del bin\CMakeLists.txt
+copy resources\bin\doc\versionhistory.txt bin
+copy resources\bin\doc\controls.txt bin
+copy resources\bin\windowed.bat bin
+copy resources\bin\d2tm.cfg bin
 echo Done.
