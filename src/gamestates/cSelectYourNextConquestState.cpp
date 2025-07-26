830b69dc98c104bb92aafd6d532657c1703171b0
Mira <olivier.nivoix@gmail.com>
2025-07-26 06:39:42 +0200

    Displays the region number

--- src/gamestates/cSelectYourNextConquestState.cpp
+++ src/gamestates/cSelectYourNextConquestState.cpp
@@ -516,6 +516,7 @@ int cSelectYourNextConquestState::REGION_OVER(int mouseX, int mouseY)
     //std::cout << "REGION_OVER " << mouseX-offsetX << " " << mouseY-offsetY << " " << c << std::endl;
     // @Mira display text
     //alfont_textprintf(bmp_screen, bene_font, 17,17, Color{0,0,0), "region %d", c-1);
+    drawManager->setMessage(fmt::format("region {}",c-1));
     return c - 1;
 }
 
