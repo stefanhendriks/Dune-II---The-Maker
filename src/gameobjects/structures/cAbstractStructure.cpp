d06aa8abfb244bc80b2b7abf68ea9863cc1e9f99
Stefan Hendriks <shendriks@keesdeboekhouder.nl>
2025-07-26 18:58:19 +0200

    Extract to constant

--- src/gameobjects/structures/cAbstractStructure.cpp
+++ src/gameobjects/structures/cAbstractStructure.cpp
@@ -905,7 +905,7 @@ void cAbstractStructure::drawWithShadow()
     cRectangle dest =  {drawX, drawY, scaledWidth, scaledHeight};
     Texture *shadow = getShadowBitmap();
     if (shadow) {
-        renderDrawer->renderStrechSprite(shadow, src, dest,160);
+        renderDrawer->renderStrechSprite(shadow, src, dest,StructureShadowTrans);
     }
     renderDrawer->renderStrechSprite(bitmapToDraw, src, dest);
 }
