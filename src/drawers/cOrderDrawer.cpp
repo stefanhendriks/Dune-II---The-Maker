1cb5927394c43d1d11d108a6a1ba132a39c5cb5b
Stefan Hendriks <shendriks@keesdeboekhouder.nl>
2025-10-28 23:38:05 +0100

    (re)naming for member variables + constructor initialization

--- src/drawers/cOrderDrawer.cpp
+++ src/drawers/cOrderDrawer.cpp
@@ -11,24 +11,26 @@
 #include "context/GraphicsContext.hpp"
 #include <SDL2/SDL.h>
 
-cOrderDrawer::cOrderDrawer(GameContext *ctx, cPlayer *thePlayer) : m_ctx(ctx), player(thePlayer)
+cOrderDrawer::cOrderDrawer(GameContext *ctx, cPlayer *player) :
+    m_ctx(ctx),
+    m_player(player)
 {
     auto *gfxinter = m_ctx->getGraphicsContext()->gfxinter.get();
-    buttonBitmap = thePlayer->createTextureFromIndexedSurfaceWithPalette(gfxinter->getSurface(BTN_ORDER), TransparentColorIndex);
-    int halfOfButton = buttonBitmap->w / 2;
+    m_buttonBitmap = player->createTextureFromIndexedSurfaceWithPalette(gfxinter->getSurface(BTN_ORDER), TransparentColorIndex);
+    int halfOfButton = m_buttonBitmap->w / 2;
     int halfOfSidebar = cSideBar::SidebarWidthWithoutCandyBar / 2;
     int halfOfHeightLeftForButton = 50 / 2; // 50 = height of 1 row icons which is removed for Starport
-    int halfOfButtonHeight = buttonBitmap->h / 2;
-    buttonRect = cRectangle((game.m_screenW - halfOfSidebar) - halfOfButton,
+    int halfOfButtonHeight = m_buttonBitmap->h / 2;
+    m_buttonRect = cRectangle((game.m_screenW - halfOfSidebar) - halfOfButton,
                             (game.m_screenH - halfOfHeightLeftForButton) - halfOfButtonHeight,
-                            buttonBitmap->w, buttonBitmap->h);
-    _isMouseOverOrderButton = false;
+                            m_buttonBitmap->w, m_buttonBitmap->h);
+    m_isMouseOverOrderButton = false;
 }
 
 cOrderDrawer::~cOrderDrawer()
 {
-    if (buttonBitmap)
-        delete buttonBitmap;
+    if (m_buttonBitmap)
+        delete m_buttonBitmap;
 }
 
 void cOrderDrawer::drawOrderButton(cPlayer *thePlayer)
@@ -39,39 +41,39 @@ void cOrderDrawer::drawOrderButton(cPlayer *thePlayer)
 
     assert(orderProcesser);
     if (orderProcesser->isOrderPlaced()) { //grey
-        renderDrawer->renderSprite(buttonBitmap, buttonRect.getX(), buttonRect.getY());
-        renderDrawer->renderRectColor(buttonRect,0,0,0,128);
+        renderDrawer->renderSprite(m_buttonBitmap, m_buttonRect.getX(), m_buttonRect.getY());
+        renderDrawer->renderRectColor(m_buttonRect,0,0,0,128);
     }
     else {
-        renderDrawer->renderSprite(buttonBitmap, buttonRect.getX(), buttonRect.getY());
+        renderDrawer->renderSprite(m_buttonBitmap, m_buttonRect.getX(), m_buttonRect.getY());
     }
 
-    if (_isMouseOverOrderButton) {
+    if (m_isMouseOverOrderButton) {
         drawRectangleOrderButton();
     }
 }
 
 void cOrderDrawer::drawRectangleOrderButton()
 {
-    int x = buttonRect.getX();
-    int y = buttonRect.getY();
-    int width = buttonRect.getWidth();
-    int height = buttonRect.getHeight();
-    Color color = player->getHouseFadingColor();
+    int x = m_buttonRect.getX();
+    int y = m_buttonRect.getY();
+    int width = m_buttonRect.getWidth();
+    int height = m_buttonRect.getHeight();
+    Color color = m_player->getHouseFadingColor();
     renderDrawer->renderRectColor(x, y, width, height, color);
     renderDrawer->renderRectColor(x+1, y+1, width-2, height-2, color);
 }
 
 void cOrderDrawer::onMouseAt(const s_MouseEvent &event)
 {
-    _isMouseOverOrderButton = buttonRect.isPointWithin(event.coords);
+    m_isMouseOverOrderButton = m_buttonRect.isPointWithin(event.coords);
 }
 
 void cOrderDrawer::onMouseClickedLeft(const s_MouseEvent &)
 {
     // handle "order" button interaction
-    if (_isMouseOverOrderButton) {
-        cOrderProcesser *orderProcesser = player->getOrderProcesser();
+    if (m_isMouseOverOrderButton) {
+        cOrderProcesser *orderProcesser = m_player->getOrderProcesser();
         if (orderProcesser->canPlaceOrder()) {
             orderProcesser->placeOrder();
         }
@@ -95,5 +97,5 @@ void cOrderDrawer::onNotify(const s_MouseEvent &event)
 
 void cOrderDrawer::setPlayer(cPlayer *pPlayer)
 {
-    this->player = pPlayer;
+    this->m_player = pPlayer;
 }
