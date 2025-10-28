0eba69d85cd98394e8c540f3c851b4a5a7f8b8b4
Mira <olivier.nivoix@gmail.com>
2025-10-28 08:29:59 +0100

    Rewrite INI_Load_scenario

--- src/ini.h
+++ src/ini.h
@@ -28,18 +28,18 @@ void INI_Load_Regionfile(int iHouse, int iMission, cSelectYourNextConquestState 
 
 
 bool INI_Scenario_Section_Units(int iHumanID, bool bSetUpPlayers, const int *iPl_credits, const int *iPl_house,
-                                 const int *iPl_quota, const char *linefeed);
+                                 const int *iPl_quota, const std::string& linefeed);
 
 bool INI_Scenario_Section_Structures(int iHumanID, bool bSetUpPlayers, const int *iPl_credits, const int *iPl_house,
-                                      const int *iPl_quota, char *linefeed);
+                                      const int *iPl_quota, const std::string& linefeed);
 
-void INI_Scenario_Section_Reinforcements(int iHouse, const char *linefeed, cReinforcements *reinforcements);
+void INI_Scenario_Section_Reinforcements(int iHouse, const std::string& linefeed, cReinforcements *reinforcements);
 
-void INI_Scenario_Section_MAP(int *blooms, int *fields, int wordtype, char *linefeed);
+void INI_Scenario_Section_MAP(int *blooms, int *fields, int wordtype, const std::string& linefeed);
 
-int INI_Scenario_Section_House(int wordtype, int iPlayerID, int *iPl_credits, int *iPl_quota, char *linefeed);
+int INI_Scenario_Section_House(int wordtype, int iPlayerID, int *iPl_credits, int *iPl_quota, const std::string& linefeed);
 
-void INI_Scenario_Section_Basic(AbstractMentat *pMentat, int wordtype, char *linefeed);
+void INI_Scenario_Section_Basic(AbstractMentat *pMentat, int wordtype, const std::string& linefeed);
 
 void INI_Scenario_SetupPlayers(int iHumanID, const int *iPl_credits, const int *iPl_house, const int *iPl_quota);
 
