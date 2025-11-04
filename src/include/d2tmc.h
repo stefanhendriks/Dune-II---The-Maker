928dee989ae1755dba38c65f4077cd36c23d3e51
Mira <mira@legendes-oubliees.org>
2025-11-04 18:29:06 +0100

    Unused definition

--- src/include/d2tmc.h
+++ src/include/d2tmc.h
@@ -60,7 +60,7 @@ extern cMap           global_map;
 
 extern cAbstractStructure     *structure[MAX_STRUCTURES];
 extern cUnit          unit[MAX_UNITS];
-extern cRandomMapGenerator randomMapGenerator;
+// extern cRandomMapGenerator randomMapGenerator;
 extern cMapCamera	  *mapCamera;
 extern cPlayer        players[MAX_PLAYERS];
 extern cParticle      particle[MAX_PARTICLES];
