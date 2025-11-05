a21ef650fd6864613008ee8e603a5d0bf74135b1
Mira <olivier.nivoix@gmail.com>
2025-11-05 06:58:09 +0100

    First step TextContext

--- src/context/ContextCreator.hpp
+++ src/context/ContextCreator.hpp
@@ -1,11 +1,14 @@
 #pragma once
 
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
 #include <memory>
 
 class cFileValidator;
 class GraphicsContext;
 class Graphics;
+class TextContext;
+class cTextDrawer;
 
 class ContextCreator {
 public:
@@ -20,4 +23,8 @@ private:
     std::shared_ptr<Graphics> gfxworld;
     std::shared_ptr<Graphics> gfxinter;
     std::shared_ptr<Graphics> gfxdata;
+
+    TTF_Font *game_font = nullptr; 	// arrakeen.fon
+    TTF_Font *bene_font = nullptr;	// benegesserit font.
+    TTF_Font *small_font = nullptr;	// small font.
 };
