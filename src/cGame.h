d2dc54b7a6b4562cebe71e18760a1b3c7be8fa58
Mira <mira@legendes-oubliees.org>
2025-11-05 18:45:52 +0100

    Migration textDrawer to ctx

--- src/cGame.h
+++ src/cGame.h
@@ -303,7 +303,7 @@ private:
     SDL_Renderer *renderer;
     Texture *screenTexture=nullptr;
     Texture *actualRenderer= nullptr;
-    std::unique_ptr<cTextDrawer> textDrawer;
+    std::shared_ptr<cTextDrawer> textDrawer = nullptr;
     cTimeManager* m_timeManager;
 
     std::shared_ptr<cHousesInfo> m_Houses;
