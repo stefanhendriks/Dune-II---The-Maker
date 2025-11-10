42d775773eba181886acdb303c5db69a41abbbf9
Mira <olivier.nivoix@gmail.com>
2025-11-10 08:04:47 +0100

    Fix  look

--- src/ini.h
+++ src/ini.h
@@ -20,38 +20,38 @@ class cSelectYourNextConquestState;
 // public stuff
 class cIni {
 public:
-// Read game.ini file
-static void INI_Install_Game(std::string filename);
-// Load original scenario ini file
-static void INI_Load_scenario(int iHouse, int iRegion, AbstractMentat *pMentat,cReinforcements *reinforcements);
-// Load briefing for original scenario
-static void INI_LOAD_BRIEFING(int iHouse, int iScenarioFind, int iSectionFind, AbstractMentat *pMentat);
+    // Read game.ini file
+    static void INI_Install_Game(std::string filename);
+    // Load original scenario ini file
+    static void INI_Load_scenario(int iHouse, int iRegion, AbstractMentat *pMentat,cReinforcements *reinforcements);
+    // Load briefing for original scenario
+    static void INI_LOAD_BRIEFING(int iHouse, int iScenarioFind, int iSectionFind, AbstractMentat *pMentat);
+    // Load regionfile for original scenario
+    static void INI_Load_Regionfile(int iHouse, int iMission, cSelectYourNextConquestState *selectYourNextConquestState);
 
-// Load regionfile for original scenario
-static void INI_Load_Regionfile(int iHouse, int iMission, cSelectYourNextConquestState *selectYourNextConquestState);
-
-// private stuff
-// Section Units, old Code style
-static bool INI_Scenario_Section_Units(int iHumanID, bool bSetUpPlayers, const int *iPl_credits, const int *iPl_house,
-                                 const int *iPl_quota, const std::string& linefeed);
-// Section Structures, old Code style
-static bool INI_Scenario_Section_Structures(int iHumanID, bool bSetUpPlayers, const int *iPl_credits, const int *iPl_house,
-                                      const int *iPl_quota, const std::string& linefeed);
-
-// Load seed for random generator to original scenario maps
-static void INI_Load_seed(int seed);
 
 private:
-// Reinforcements, old Code style
-static void INI_Scenario_Section_Reinforcements(int iHouse, const std::string& linefeed, cReinforcements *reinforcements);
-// Map, old Code style
-static void INI_Scenario_Section_MAP(int *blooms, int *fields, int wordtype, const std::string& linefeed);
-// Section House, old Code style
-static int INI_Scenario_Section_House(int wordtype, int iPlayerID, int *iPl_credits, int *iPl_quota, const std::string& linefeed);
-// Section_Basic, old Code style
-static void INI_Scenario_Section_Basic(AbstractMentat *pMentat, int wordtype, const std::string& linefeed);
-// SetupPlayers, old Code style
-static void INI_Scenario_SetupPlayers(int iHumanID, const int *iPl_credits, const int *iPl_house, const int *iPl_quota);
+    // Section Units, old Code style
+    static bool INI_Scenario_Section_Units(int iHumanID, bool bSetUpPlayers, const int *iPl_credits, const int *iPl_house,
+                                     const int *iPl_quota, const std::string& linefeed);
+    // Section Structures, old Code style
+    static bool INI_Scenario_Section_Structures(int iHumanID, bool bSetUpPlayers, const int *iPl_credits, const int *iPl_house,
+                                          const int *iPl_quota, const std::string& linefeed);
+
+    // Load seed for random generator to original scenario maps
+    static void INI_Load_seed(int seed);
+
+
+    // Reinforcements, old Code style
+    static void INI_Scenario_Section_Reinforcements(int iHouse, const std::string& linefeed, cReinforcements *reinforcements);
+    // Map, old Code style
+    static void INI_Scenario_Section_MAP(int *blooms, int *fields, int wordtype, const std::string& linefeed);
+    // Section House, old Code style
+    static int INI_Scenario_Section_House(int wordtype, int iPlayerID, int *iPl_credits, int *iPl_quota, const std::string& linefeed);
+    // Section_Basic, old Code style
+    static void INI_Scenario_Section_Basic(AbstractMentat *pMentat, int wordtype, const std::string& linefeed);
+    // SetupPlayers, old Code style
+    static void INI_Scenario_SetupPlayers(int iHumanID, const int *iPl_credits, const int *iPl_house, const int *iPl_quota);
 
 
 };
