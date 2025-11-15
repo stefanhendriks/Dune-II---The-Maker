1fb717fd4889592c6dccd5d8bdb1091830dbb915
Mira <olivier.nivoix@gmail.com>
2025-11-15 08:57:20 +0100

    Create TellHouse

---
+++ src/gamestates/cTellHouseState.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "cGameState.h"
+#include "mentat/AbstractMentat.h"
+
+
+class cTellHouseState : public cGameState {
+public:
+    cTellHouseState(cGame &game, GameContext* ctx, int house = -1);
+    ~cTellHouseState() override;
+
+    void thinkFast() override;
+    void draw() const override;
+
+    void onNotifyMouseEvent(const s_MouseEvent &event) override;
+    void onNotifyKeyboardEvent(const cKeyboardEvent &event) override;
+
+    eGameStateType getType() override { return eGameStateType::GAMESTATE_MAIN_MENU; } // À adapter si besoin
+    void prepareMentat(int house);
+
+private:
+    AbstractMentat* m_mentat = nullptr;
+    int m_house = -1;
+};
