c76151e4e0802cb8a1ffd0dc348b019c133c5a78
Mira <olivier.nivoix@gmail.com>
2025-11-15 10:46:02 +0100

    Code clean

--- src/utils/cScreenShake.cpp
+++ src/utils/cScreenShake.cpp
@@ -6,25 +6,25 @@
 #include "utils/RNG.hpp"
 
 cScreenShake::cScreenShake()
-    : m_shakeX(0), m_shakeY(0), m_TIMER_shake(0)
+    : m_shakeX(0), m_shakeY(0), m_timer(0)
 {}
 
 void cScreenShake::shake(int duration)
 {
-    m_TIMER_shake += duration;
+    m_timer += duration;
 }
 
 void cScreenShake::reduce()
 {
-    if (m_TIMER_shake > 0)
-        m_TIMER_shake--;
+    if (m_timer > 0)
+        m_timer--;
 }
 
 void cScreenShake::reset()
 {
     m_shakeX = 0;
     m_shakeY = 0;
-    m_TIMER_shake = 0;
+    m_timer = 0;
 }
 
 int cScreenShake::getX() const
@@ -39,28 +39,23 @@ int cScreenShake::getY() const
 
 int cScreenShake::getTimer() const
 {
-    return m_TIMER_shake;
+    return m_timer;
 }
 
 void cScreenShake::update(int state, int playingState, cMapCamera *mapCamera)
 {
-    if (m_TIMER_shake == 0) {
-        m_TIMER_shake = -1;
+    if (m_timer == 0) {
+        m_timer = -1;
     }
 
-    if (m_TIMER_shake > 0 && state == playingState) {
+    if (m_timer > 0 && state == playingState) {
         // the more we get to the 'end' the less we 'throttle'.
         // Structure explosions are 6 time units per cell.
         // Max is 9 cells (9*6=54)
         // the max border is then 9. So, we do time / 6
-        int shakiness = std::min(m_TIMER_shake, 69);
+        int shakiness = std::min(m_timer, 69);
         float offset = mapCamera->factorZoomLevel(std::min(shakiness / 5, 9));
         m_shakeX = -abs(offset / 2) + RNG::rnd(offset);
         m_shakeY = -abs(offset / 2) + RNG::rnd(offset);
     } 
-    // else {
-        // m_shakeX = 0;
-        // m_shakeY = 0;
-        // m_TIMER_shake = -1;
-    // }
 }
