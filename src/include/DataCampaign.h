77962fd8dc06164b83416d908d173165b68531d4
Mira <olivier.nivoix@gmail.com>
2025-11-17 07:26:32 +0100

    Create DataCampaign

---
+++ src/include/DataCampaign.h
@@ -0,0 +1,7 @@
+#pragma once
+
+struct dataCampaign {
+    int housePlayer;
+    int m_region;                   // what region is selected?
+    int m_mission;		            // what mission are we playing?
+};
