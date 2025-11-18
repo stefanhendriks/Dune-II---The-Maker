5591ffab8d0a1c97758567873dc85b42cf7e3892
Mira <olivier.nivoix@gmail.com>
2025-11-18 23:30:16 +0100

    Partrial test of MentatState (NC)

---
+++ src/gamestates/cMentatState.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "cGameState.h"
+#include "mentat/AbstractMentat.h"
+
+struct s_DataCampaign;
+
+enum class MentatMode {
+    Briefing,
+    WinBrief,
+    LoseBrief
+};
+
+class cMentatState : public cGameState {
+public:
+    cMentatState(cGame &game, GameContext* ctx, MentatMode mode, s_DataCampaign* dataCampaign, int house = -1);
+    ~cMentatState() override;
+
+    void thinkFast() override;
+    void draw() const override;
+
+    void onNotifyMouseEvent(const s_MouseEvent &event) override;
+    void onNotifyKeyboardEvent(const cKeyboardEvent &event) override;
+
+    eGameStateType getType() override { return eGameStateType::GAMESTATE_MAIN_MENU; } // À adapter si besoin
+
+private:
+    AbstractMentat* m_mentat = nullptr;
+    s_DataCampaign* m_dataCampaign = nullptr;
+    MentatMode m_mode;
+    int m_house;
+    void prepareMentat();
+};
