b07501093a569911ec346a81bbe642256f330993
Mira <mira@legendes-oubliees.org>
2025-11-22 15:12:36 +0100

    Rename method

--- src/cGame_logic.cpp
+++ src/cGame_logic.cpp
@@ -1272,7 +1272,7 @@ void cGame::goingToWinLoseBrief(int value)
 
 }
 
-void cGame::execute()
+void cGame::changeStateFromMentat()
 {
     if (game.isState(GAME_BRIEFING)) {
         // proceed, play mission (it is already loaded before we got here)
