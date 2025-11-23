768962b818bb5b124c899e5792ee5ae9821533e6
Mira <mira@legendes-oubliees.org>
2025-11-23 13:35:39 +0100

    Fix language

--- src/gamestates/cGameState.cpp
+++ src/gamestates/cGameState.cpp
@@ -10,29 +10,29 @@ cGameState::cGameState(cGame &theGame, GameContext* _ctx) :
 std::string gameStateToString(GameState state)
 {
     switch (state) {
-        case GameState::INITIALIZE: return "initialize game";
-        case GameState::OVER: return "game over";
-        case GameState::MENU: return "in a menu";
-        case GameState::PLAYING: return "playing the actual game";
-        case GameState::BRIEFING: return "doing some briefing";
-        case GameState::EDITING: return "running the editor";
-        case GameState::OPTIONS: return "options menu";
-        case GameState::REGION: return "select a region / select your next conquest";
-        case GameState::SELECT_HOUSE: return "pick a house";
-        case GameState::INTRO: return "intro";
-        case GameState::TELLHOUSE: return "tell about the house";
-        case GameState::WINNING: return "when winning, do some cool animation";
-        case GameState::WINBRIEF: return "mentat chatter when won the mission";
-        case GameState::LOSEBRIEF: return "briefing when losing";
-        case GameState::LOSING: return "when losing, do something cool";
-        case GameState::SKIRMISH: return "playing a skirmish mission!";
-        case GameState::SETUPSKIRMISH: return "set up a skirmish game";
-        case GameState::CREDITS: return "credits";
-        case GameState::MISSIONSELECT: return "mission select";
-        // COUNT n'est pas un état valide pour le jeu, mais pour l'itération.
-        // On peut lever une erreur ou retourner une chaîne spéciale.
+        case GameState::INITIALIZE: return "INITIALIZE";
+        case GameState::OVER: return "OVER";
+        case GameState::MENU: return "MENU";
+        case GameState::PLAYING: return "PLAYING";
+        case GameState::BRIEFING: return "BRIEFING";
+        case GameState::EDITING: return "EDITING";
+        case GameState::OPTIONS: return "OPTIONS";
+        case GameState::REGION: return "REGION";
+        case GameState::SELECT_HOUSE: return "SELECT_HOUSE";
+        case GameState::INTRO: return "INTRO";
+        case GameState::TELLHOUSE: return "TELLHOUSE";
+        case GameState::WINNING: return "WINNING";
+        case GameState::WINBRIEF: return "WINBRIEF";
+        case GameState::LOSEBRIEF: return "LOSEBRIEF";
+        case GameState::LOSING: return "LOSING";
+        case GameState::SKIRMISH: return "SKIRMISH";
+        case GameState::SETUPSKIRMISH: return "SETUPSKIRMISH";
+        case GameState::CREDITS: return "CREDITS";
+        case GameState::MISSIONSELECT: return "MISSIONSELECT";
+        // COUNT is not a valid state for the game, but it is for the iteration.
+        // We return a special string. Maybe an out of range error is better ?
         case GameState::COUNT: return "[Error: GameState::COUNT]";
     }
-    // Gestion des valeurs non reconnues (si l'enum est casté à partir d'un char arbitraire)
-    throw std::out_of_range("Unknown GameState value.");
+    // Handling unrecognized values ​​(if the enum is cast from an arbitrary character)
+    throw std::out_of_range("Unknown GameState value");
 }
