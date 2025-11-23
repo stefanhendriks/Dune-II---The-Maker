3af7d683753332594cc3f1bd5ab21c3dbcf1177b
Mira <olivier.nivoix@gmail.com>
2025-11-23 08:37:49 +0100

    Enum class GameState

--- src/include/enums.h
+++ src/include/enums.h
@@ -350,3 +350,26 @@ enum class SmudgeType : char {
     S_WALL = 2,
     MAX
 };
+
+enum class GameState : char {
+    INITIALIZE,            // initialize game
+    OVER,                  // game over
+    MENU,                  // in a menu
+    PLAYING,               // playing the actual game
+    BRIEFING,              // doing some briefing
+    EDITING,               // running the editor
+    OPTIONS,               // options menu
+    REGION,                // select a region / select your next conquest
+    SELECT_HOUSE,          // pick a house
+    INTRO,               
+    TELLHOUSE,             // tell about the house
+    WINNING,               // when winning, do some cool animation
+    WINBRIEF,              // mentat chatter when won the mission
+    LOSEBRIEF,             // briefing when losing
+    LOSING,                // when losing, do something cool
+    SKIRMISH,              // playing a skirmish mission!
+    SETUPSKIRMISH,         // set up a skirmish game
+    CREDITS,               // credits
+    MISSIONSELECT,         // mission select
+    MAX            
+};
