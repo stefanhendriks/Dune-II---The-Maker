555907074c01ae5c3c0fefd19127ab1d97a52194
Mira <olivier.nivoix@gmail.com>
2025-11-23 09:05:25 +0100

    Refined prototype

--- src/gamestates/cCreatorState.h
+++ src/gamestates/cCreatorState.h
@@ -4,14 +4,18 @@
 #include "gamestates/cGameState.h"
 #include "utils/cEnumArray.h"
 
+#include <memory>
+#include <optional>
+
 class CreatorState {
 public:
     explicit CreatorState();
     ~CreatorState();
 
-    cGameState *getState(GameState gameState) const;
+    cGameState *getState(GameState gameState);
 private:
     //cGameState *m_states[GameState::MAX];
-    EnumArray<cGameState *,GameState> m_states;
+    EnumArray<std::optional<std::unique_ptr<cGameState>>,GameState> m_states;
     EnumArray<bool, GameState> needToRecreateState;
+    void createStateFromScratch(GameState gameState);
 };
