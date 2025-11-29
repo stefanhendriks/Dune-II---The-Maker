f6d748ffd597771053a013188d70c1cc1f378290
Mira <olivier.nivoix@gmail.com>
2025-11-29 15:56:40 +0100

    GamePlaying skeletton

---
+++ src/gamestates/cGamePlaying.cpp
@@ -0,0 +1,43 @@
+#include "gamestates/cGamePlaying.h"
+
+#include "d2tmc.h"
+#include "config.h"
+#include "data/gfxinter.h"
+#include "data/gfxdata.h"
+#include "drawers/SDLDrawer.hpp"
+#include "utils/Graphics.hpp"
+#include "context/GameContext.hpp"
+#include "context/GraphicsContext.hpp"
+#include <SDL2/SDL.h>
+#include <format>
+
+cGamePlaying::cGamePlaying(cGame &theGame, GameContext* ctx) : cGameState(theGame, ctx)
+{
+}
+
+cGamePlaying::~cGamePlaying()
+{
+
+}
+
+void cGamePlaying::thinkFast()
+{
+}
+
+void cGamePlaying::draw() const
+{
+}
+
+void cGamePlaying::onNotifyMouseEvent(const s_MouseEvent &event)
+{
+}
+
+eGameStateType cGamePlaying::getType()
+{
+    return GAMESTATE_PLAYING;
+}
+
+void cGamePlaying::onNotifyKeyboardEvent(const cKeyboardEvent &event)
+{
+
+}
