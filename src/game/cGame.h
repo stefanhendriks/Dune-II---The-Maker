b1c9c097eee0832e5f6132a77f259c84e6c26fda
Mira <olivier.nivoix@gmail.com>
2025-11-29 11:34:14 +0100

    Remove unused var

--- src/game/cGame.h
+++ src/game/cGame.h
@@ -93,7 +93,6 @@ public:
 
     bool m_playing;				    // playing or not
     bool m_skirmish;                // playing a skirmish game or not
-    int m_screenshot;				// screenshot taking number
 
     int m_pathsCreated;
 
