6d697ad85eb024a5c3b9cb75e1175e51d41c84b3
Mira <olivier.nivoix@gmail.com>
2025-11-29 08:37:34 +0100

    Code cosmetic

--- src/game/cGame.h
+++ src/game/cGame.h
@@ -320,8 +320,6 @@ private:
 
     bool m_missionWasWon;               // hack: used for state transitioning :/
 
-    int m_state;
-
     int m_newMusicSample;
     int m_newMusicCountdown;
 
@@ -338,12 +336,13 @@ private:
     // win/lose flags
     int8_t m_winFlags, m_loseFlags;
 
+    int m_state;
     int m_nextState;
-
     // the current game state we are running
     cGameState *m_currentState;
-
     cGameState *m_states[GAME_MAX_STATES];
+    void transitionStateIfRequired();
+    void setState(int newState);
 
     void updateMouseAndKeyboardState();
     void updateGamePlaying();
@@ -369,10 +368,6 @@ private:
     [[nodiscard]] bool allEnemyAIPlayersAreDestroyed() const;
 
     [[nodiscard]] bool hasGameOverConditionAIHasNoBuildings() const;
-
-    void transitionStateIfRequired();
-
-    void setState(int newState);
 
     void saveBmpScreenToDisk();
 
