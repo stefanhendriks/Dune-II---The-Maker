d90c623cd38e297835dedb5c53411bdf15635716
Mira <mira@legendes-oubliees.org>
2025-12-08 19:06:50 +0100

    add GAME_EVENT_GAMESTATE

--- src/include/sGameEvent.cpp
+++ src/include/sGameEvent.cpp
@@ -47,6 +47,8 @@ const char *s_GameEvent::toString(const eGameEventType &eventType)
             return "GAME_EVENT_PLAYER_DEFEATED";
         case eGameEventType::GAME_EVENT_SPECIAL_LAUNCHED:
             return "GAME_EVENT_SPECIAL_LAUNCHED";
+        case eGameEventType::GAME_EVENT_GAMESTATE:
+            return "GAME_EVENT_GAMESTATE";
         default:
             assert(false && "Unknown game event type for toString()");
             break;
