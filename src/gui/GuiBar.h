f10367fdc965caf78350af178fb8277dcad2b1ff
Mira <mira@legendes-oubliees.org>
2025-12-14 20:23:56 +0100

    Create GuiBar

---
+++ src/gui/GuiBar.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "GuiObject.h"
+#include "drawers/cTextDrawer.h"
+
+#include <string>
+#include <vector>
+
+enum class GuiBarPlacement :char {
+    HORIZONTAL = 0,
+    VERTICAL = 1
+};
+
+class GuiBar : protected GuiObject {
+public:
+    explicit GuiBar(const cRectangle &rect, GuiBarPlacement placement);
+    ~GuiBar() noexcept override;
+
+    void onNotifyMouseEvent(const s_MouseEvent &event) override;
+    void onNotifyKeyboardEvent(const cKeyboardEvent &event) override;
+
+    void draw() const override;
+
+    void addGuiObject(GuiObject *guiObject);
+
+    cRectangle getRelativeRect(int x, int y, int width, int height);
+
+    void setTheme(const GuiTheme& _theme) {
+        GuiObject::setTheme(_theme);
+    }
+private:
+    std::vector<GuiObject *> gui_objects;
+    GuiBarPlacement m_placement;
+    int placementPosition;
+};
+
