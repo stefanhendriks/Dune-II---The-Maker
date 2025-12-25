3408608d757d5a21ed38db56bb8b75c0b49a953e
Mira <mira@legendes-oubliees.org>
2025-12-25 08:45:23 +0100

    Remove clampCameraToMapBounds

--- src/gamestates/cEditorState.h
+++ src/gamestates/cEditorState.h
@@ -32,7 +32,7 @@ private:
     void populateModifBar();
     void populateSelectBar();
     void drawMap() const;
-    void clampCameraToMapBounds();
+    // void clampCameraToMapBounds();
     void clampCameraXToMapBounds();
     void clampCameraYToMapBounds();
 
