692be60d5ba10c5f9daf046106ace32c18a23223
Mira <mira@legendes-oubliees.org>
2026-01-03 15:05:54 +0100

    Create loadMapFromEditor

--- src/game/cGame_logic.cpp
+++ src/game/cGame_logic.cpp
@@ -1718,3 +1718,10 @@ void cGame::loadMapFromEditor(int map)
     s_PreviewMap *selectedMap = &m_PreviewMaps->getMap(map);
     pState->loadMap(selectedMap);
 }
+
+void cGame::loadMapFromEditor(s_PreviewMap *map)
+{
+    setState(GAME_EDITOR);
+    auto *pState = dynamic_cast<cEditorState*>(m_states[GAME_EDITOR]);
+    pState->loadMap(map);
+}
