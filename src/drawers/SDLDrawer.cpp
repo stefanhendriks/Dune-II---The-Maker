6002fbc46fc59f1a6b73df9108631c0cb290920d
Mira <mira@legendes-oubliees.org>
2026-02-08 19:00:55 +0100

    Remove old code

--- src/drawers/SDLDrawer.cpp
+++ src/drawers/SDLDrawer.cpp
@@ -9,10 +9,6 @@
 
 SDLDrawer::SDLDrawer(SDL_Renderer *_renderer)
 {
-    // colorBlack= Color{0,0,0,255};
-    // gui_colorWindow = Color{176,176,196,255};
-    // gui_colorBorderDark = Color{84,84,120,255};
-    // gui_colorBorderLight = Color{252,252,252,255};
     renderer = _renderer;
 }
 
@@ -134,13 +130,6 @@ void SDLDrawer::setClippingFor(int topLeftX, int topLeftY, int bottomRightX, int
     SDL_RenderSetClipRect(renderer, &tmp);
 }
 
-// void SDLDrawer::gui_DrawRect(const cRectangle &rectangle)
-// {
-//     //gui_DrawRect(rectangle, gui_colorWindow, gui_colorBorderLight, gui_colorBorderDark);
-//     gui_DrawRect(rectangle, Color{176,176,196,255},Color{252,252,252,255},  Color{84,84,120,255});
-//         // Color{96,0,0,255}, Color{196,0,0,255}, Color{0,0,196,255});
-// }
-
 void SDLDrawer::gui_DrawRect(const cRectangle &rectangle, Color gui_colorWindow, Color gui_colorBorderLight, Color gui_colorBorderDark)
 {
     int x1= rectangle.getX();
@@ -215,47 +204,6 @@ bool SDLDrawer::isSurface8BitPaletted(SDL_Surface *bmp)
     return bmp->format->BitsPerPixel == 8 && bmp->format->palette != nullptr;
 }
 
-// void SDLDrawer::shimmer(SDL_Surface *src, int r, int x, int y, float cameraZoom)
-// {
-//     int x1, y1;
-//     int nx, ny;
-//     int gp = 0;     // Get Pixel Result
-//     int tc = 0;
-
-//     // go around 360 fDegrees (twice as fast now)
-//     float step = std::fmax(1.0f, cameraZoom);
-
-//     for (float dr = 0; dr < r; dr += step) {
-//         for (double d = 0; d < 360; d++) {
-//             x1 = (x + (cos(d) * (dr)));
-//             y1 = (y + (sin(d) * (dr)));
-
-//             if (x1 < 0) x1 = 0;
-//             if (y1 < 0) y1 = 0;
-//             if (x1 >= game.m_screenW) x1 = game.m_screenW - 1;
-//             if (y1 >= game.m_screenH) y1 = game.m_screenH - 1;
-
-//             gp = get_pixel(src, x1, y1); //ALLEGRO // use this inline function to speed up things.
-//             // Now choose random spot to 'switch' with.
-//             nx = (x1 - 1) + RNG::rnd(2);
-//             ny = (y1 - 1) + RNG::rnd(2);
-
-//             if (nx < 0) nx = 0;
-//             if (ny < 0) ny = 0;
-//             if (nx >= game.m_screenW) nx = game.m_screenW - 1;
-//             if (ny >= game.m_screenH) ny = game.m_screenH - 1;
-
-//             tc = get_pixel(src, nx, ny); //ALLEGRO
-
-//             if (gp > -1 && tc > -1) {
-//                 // Now switch colors
-//                 set_pixel(src, nx, ny, gp);
-//                 set_pixel(src, x1, y1, tc);
-//             }
-//         }
-//     }
-// }
-
 // Fonction utilitaire pour dessiner un pixel sur une SDL_Surface
 // Assurez-vous que la surface est verrouillée avant d'appeler cette fonction !
 void SDLDrawer::set_pixel(SDL_Surface *surface, int x, int y, Uint32 pixel_color)
@@ -297,49 +245,6 @@ void SDLDrawer::set_pixel(SDL_Surface *surface, int x, int y, Uint32 pixel_color
     }
 }
 
-// // Fonction utilitaire pour obtenir la couleur d'un pixel sur une SDL_Surface.
-// // Retourne la valeur du pixel (Uint32) ou 0 si hors limites/erreur.
-// // Assurez-vous que la surface est verrouillée AVANT d'appeler cette fonction si nécessaire !
-// Uint32 SDLDrawer::get_pixel(SDL_Surface *surface, int x, int y)
-// {
-//     // Vérifier les limites de la surface
-//     if (x < 0 || x >= surface->w || y < 0 || y >= surface->h) {
-//         // Pixel hors des limites, retourner 0 ou une valeur d'erreur définie
-//         // Dans un vrai programme, vous pourriez logger une erreur.
-//         return 0;
-//     }
-
-//     int bpp = surface->format->BytesPerPixel; // Bytes Per Pixel
-
-//     // Calculer l'adresse du pixel
-//     // p pointe vers le début de la ligne 'y'
-//     Uint8 *p = (Uint8 *)surface->pixels + y * surface->pitch;
-//     // Déplacer p à la position 'x' du pixel en tenant compte du BPP
-//     p += x * bpp;
-
-//     // Lire la valeur du pixel en fonction du nombre d'octets par pixel
-//     switch (bpp) {
-//         case 1: // 8 bits par pixel
-//             return *p;
-//         case 2: // 16 bits par pixel
-//             return *(Uint16 *)p;
-//         case 3: // 24 bits par pixel (spécial car non aligné sur 4 octets)
-//             // L'ordre des octets dépend de l'endianness du système
-//             if (SDL_BYTEORDER == SDL_BIG_ENDIAN) {
-//                 return (p[0] << 16) | (p[1] << 8) | p[2];
-//             }
-//             else {
-//                 return p[0] | (p[1] << 8) | (p[2] << 16);
-//             }
-//         case 4: // 32 bits par pixel
-//             return *(Uint32 *)p;
-//         default:
-//             fprintf(stderr, "Format de pixel non supporté pour get_pixel: %d BPP\n", bpp);
-//             return 0; // Ou une valeur d'erreur
-//     }
-// }
-
-
 void SDLDrawer::FillWithColor(SDL_Surface *src, Color color)
 {
     Uint32 mappedColor = SDL_MapRGBA(src->format,
@@ -368,23 +273,6 @@ void SDLDrawer::setPixel(SDL_Surface *bmp, int x, int y, Color color)
     SDL_UnlockSurface(bmp);
 }
 
-
-// Color SDLDrawer::getPixel(SDL_Surface *surface, int x, int y)
-// {
-//     if (SDL_LockSurface(surface) < 0) {
-//         fprintf(stderr, "Error locking surface: %s\n", SDL_GetError());
-//         return Color{0,0,0,255};
-//     }
-//     auto tmpColor = get_pixel(surface,x,y);
-//     SDL_UnlockSurface(surface);
-//     Color extractedColor;
-//     SDL_GetRGBA(tmpColor, surface->format,
-//                 &extractedColor.r,
-//                 &extractedColor.g,
-//                 &extractedColor.b,
-//                 &extractedColor.a);
-//     return extractedColor;
-// }
 
 void SDLDrawer::renderChangeColor(Color color)
 {
