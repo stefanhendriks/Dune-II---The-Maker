7b0ef4b7aedbfd30ca682bf4c5f219412d846574
Mira <mira@legendes-oubliees.org>
2026-02-18 17:31:53 +0100

    Fix g++ toolchain

--- install_dependencies_ubuntu.sh
+++ install_dependencies_ubuntu.sh
@@ -8,4 +8,7 @@ echo  ---  Installing dependencies  ---
 echo  ---                           ---
 echo  ---------------------------------
 
+
+sudo apt-get install g++
+sudo apt-get install make cmake
 sudo apt-get install libsdl2-dev libsdl2-image-dev libsdl2-ttf-dev libsdl2-mixer-dev
