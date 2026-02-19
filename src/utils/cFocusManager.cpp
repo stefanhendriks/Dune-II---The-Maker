b9e32ff83fdbefd5ead2b683ac2f04146562f575
Mira <mira@legendes-oubliees.org>
2026-02-19 07:14:43 +0100

    Verbose test on focus

--- src/utils/cFocusManager.cpp
+++ src/utils/cFocusManager.cpp
@@ -1,5 +1,6 @@
 #include "utils/cFocusManager.h"
 #include "utils/cTimeManager.h"
+#include <iostream>
 
 cFocusManager::cFocusManager(cTimeManager* timeManager)
 {
@@ -36,22 +37,22 @@ void cFocusManager::onWindowsFocus(const SDL_WindowEvent &event)
         case SDL_WINDOWEVENT_FOCUS_LOST:
             hasFocus = false;
             m_timeManager->focusLost();
-            // std::cout << "Window focus lost" << std::endl;
+            std::cout << "Window focus lost" << std::endl;
             break;
         case SDL_WINDOWEVENT_FOCUS_GAINED:
             hasFocus = true;
             m_timeManager->focusGained();
-            // std::cout << "Window focus gained" << std::endl;
+            std::cout << "Window focus gained" << std::endl;
             break;
         case SDL_WINDOWEVENT_MINIMIZED:
             hasFocus = false;
             m_timeManager->focusLost();
-            // std::cout << "Window minimized" << std::endl;
+            std::cout << "Window minimized" << std::endl;
             break;
         case SDL_WINDOWEVENT_RESTORED:
             hasFocus = true;
             m_timeManager->focusGained();
-            // std::cout << "Window restored" << std::endl;
+            std::cout << "Window restored" << std::endl;
             break;
     }
 }
