62462ddd600417e4abcbbae964e2b38e054b316d
Mira <mira@legendes-oubliees.org>
2026-03-23 21:06:04 +0100

    Move const values

--- src/include/d2tmc.h
+++ src/include/d2tmc.h
@@ -32,15 +32,7 @@
 #include <array>
 #include <SDL2/SDL_ttf.h>
 
-/**
- * This is the palette index (in the 8 bit palette of D2TM/Dune 2) that is meant to be rendered as a 'shadow'
- */
-constexpr int TransparentColorIndex = 223;
 
-/**
- * The transparency (value between 0-255) when rendering a bitmap/texture with transparency
- */
-constexpr int ShadowTrans = 160;
 
 class cAbstractStructure;
 class SDLDrawer;
