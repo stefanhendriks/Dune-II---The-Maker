0f43e29f715ad37dd029ddbb0433ab511c895f1d
Mira <mira@legendes-oubliees.org>
2026-03-28 06:41:20 +0100

    Create cPathFinder

---
+++ src/gameobjects/units/cPathFinder.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <vector>
+
+class cPathFinder {
+public:
+    //std::vector<int> createPath(int startCell, int goalCell);
+    static int createPath(int iUnitId, int iPathCountUnits);
+private: 
+
+};
