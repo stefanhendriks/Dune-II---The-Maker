04ee8943bfe0d10242d93aa9b5d9150fe048a029
Mira <mira@legendes-oubliees.org>
2026-04-05 19:13:06 +0200

    Fix conformity

--- src/gameobjects/projectiles/cBullets.h
+++ src/gameobjects/projectiles/cBullets.h
@@ -2,6 +2,7 @@
 
 #include <array>
 #include <cassert>
+#include <cstddef>
 #include "definitions.h"
 #include "gameobjects/projectiles/bullet.h"
 
@@ -11,25 +12,22 @@
  */
 class cBullets {
 public:
-    using value_type = cBullet;
-    using size_type = std::size_t;
-
     cBullets() = default;
 
-    value_type &operator[](size_type index) {
+    cBullet &operator[](std::size_t index) {
         assert(index < MAX_BULLETS && "Index out of bounds for g_Bullet");
         return m_values[index];
     }
 
-    const value_type &operator[](size_type index) const {
+    const cBullet &operator[](std::size_t index) const {
         assert(index < MAX_BULLETS && "Index out of bounds for g_Bullet");
         return m_values[index];
     }
 
-    value_type *data() noexcept { return m_values.data(); }
-    const value_type *data() const noexcept { return m_values.data(); }
+    cBullet *data() noexcept { return m_values.data(); }
+    const cBullet *data() const noexcept { return m_values.data(); }
 
-    size_type size() const noexcept { return m_values.size(); }
+    std::size_t size() const noexcept { return m_values.size(); }
 
     auto begin() noexcept { return m_values.begin(); }
     auto end() noexcept { return m_values.end(); }
@@ -38,5 +36,5 @@ public:
 
 private:
     static constexpr std::size_t MAX_BULLETS = 300;     // max of bullets in the game at once
-    std::array<value_type, MAX_BULLETS> m_values{};
+    std::array<cBullet, MAX_BULLETS> m_values{};
 };
