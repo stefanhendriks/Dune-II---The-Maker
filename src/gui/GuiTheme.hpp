0bc51aded768b02f50d66b91dcbc2784894876a1
Stefan Hendriks <stefan@fundynamic.com>
2026-04-05 15:04:54 +0200

    Move dark and inactive theme into builder

--- src/gui/GuiTheme.hpp
+++ src/gui/GuiTheme.hpp
@@ -13,38 +13,37 @@ struct GuiTheme {
     Color textColorShadow;
     Color disabled;
 
-    static GuiTheme Dark() {
-        return {
-            Color{84, 84, 120,255},
-            Color{40, 40, 40,255},
-            Color{176, 176, 196,255},
-            Color{32, 32, 32,255},
-            Color::white(),
-            Color{225, 177, 21,255},
-            Color{255, 0, 0,255},
-            Color::black(),
-            Color{128, 128, 128,255}
-        };
-    }
-
-    static GuiTheme Inactive() {
-        return {
-            Color{84, 84, 120,255},
-            Color{40, 40, 40,255},
-            Color{176, 176, 196,255},
-            Color{32, 32, 32,255},
-            Color{192, 192, 192,255},
-            Color{128, 128, 128,255},
-            Color::black(),
-            Color{128, 128, 128,255},
-            Color{128, 128, 128,255}
-        };
-    }
 };
 
 class cGuiThemeBuilder {
 
 public:
+
+    cGuiThemeBuilder &dark() {
+        m_fillColor = Color{84, 84, 120, 255};
+        m_borderLight = Color{40, 40, 40, 255};
+        m_borderDark = Color{176, 176, 196, 255};
+        m_background = Color{32, 32, 32, 255};
+        m_textColor = Color::white();
+        m_textDarkColor = Color{225, 177, 21, 255};
+        m_textColorHover = Color{255, 0, 0, 255};
+        m_textColorShadow = Color::black();
+        m_disabled = Color{128, 128, 128, 255};
+        return *this;
+    }
+
+    cGuiThemeBuilder &inactive() {
+        m_fillColor = Color{84, 84, 120, 255};
+        m_borderLight = Color{40, 40, 40, 255};
+        m_borderDark = Color{176, 176, 196, 255};
+        m_background = Color{32, 32, 32, 255};
+        m_textColor = Color{192, 192, 192, 255};
+        m_textDarkColor = Color{128, 128, 128, 255};
+        m_textColorHover = Color::black();
+        m_textColorShadow = Color{128, 128, 128, 255};
+        m_disabled = Color{128, 128, 128, 255};
+        return *this;
+    }
 
     cGuiThemeBuilder &light() {
         m_fillColor = Color{176, 176, 196, 255};
