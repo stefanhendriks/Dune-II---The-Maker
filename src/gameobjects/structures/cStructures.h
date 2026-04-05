98897514aa4180fba53ed9e4d8372f1ae151ef69
Mira <mira@legendes-oubliees.org>
2026-04-05 19:07:55 +0200

    Fix conformity

--- src/gameobjects/structures/cStructures.h
+++ src/gameobjects/structures/cStructures.h
@@ -2,6 +2,7 @@
 
 #include <array>
 #include <cassert>
+#include <cstddef>
 #include "definitions.h"
 #include "gameobjects/structures/cAbstractStructure.h"
 
@@ -11,25 +12,22 @@
  */
 class cStructures {
 public:
-    using value_type = cAbstractStructure*;
-    using size_type = std::size_t;
-
     cStructures() = default;
 
-    value_type &operator[](size_type index) {
-        assert(index < MAX_STRUCTURES && "Index out of bounds for g_pStructure");
+    cAbstractStructure* &operator[](std::size_t index) {
+        assert(index < MAX_STRUCTURES && "Index out of bounds for cStructure");
         return m_values[index];
     }
 
-    const value_type &operator[](size_type index) const {
-        assert(index < MAX_STRUCTURES && "Index out of bounds for g_pStructure");
+    cAbstractStructure* const &operator[](std::size_t index) const {
+        assert(index < MAX_STRUCTURES && "Index out of bounds for cStructure");
         return m_values[index];
     }
 
-    value_type *data() noexcept { return m_values.data(); }
-    const value_type *data() const noexcept { return m_values.data(); }
+    cAbstractStructure** data() noexcept { return m_values.data(); }
+    const cAbstractStructure* const* data() const noexcept { return m_values.data(); }
 
-    size_type size() const noexcept { return m_values.size(); }
+    std::size_t size() const noexcept { return m_values.size(); }
 
     auto begin() noexcept { return m_values.begin(); }
     auto end() noexcept { return m_values.end(); }
@@ -38,5 +36,5 @@ public:
 
 private:
     static constexpr std::size_t MAX_STRUCTURES_COUNT = MAX_STRUCTURES;
-    std::array<value_type, MAX_STRUCTURES_COUNT> m_values{};
+    std::array<cAbstractStructure*, MAX_STRUCTURES_COUNT> m_values{};
 };
