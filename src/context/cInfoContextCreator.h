71d8dc273ee0aad756b9294caf45d3bf6ca47ad0
Mira <mira@legendes-oubliees.org>
2026-04-06 22:05:37 +0200

    Move initialisation on cInfoContextCreator

--- src/context/cInfoContextCreator.h
+++ src/context/cInfoContextCreator.h
@@ -12,11 +12,10 @@ class SDL_Renderer;
 
 class cInfoContextCreator {
 public:
-    cInfoContextCreator(SDL_Renderer *renderer);
-    cInfoContextCreator() = delete;
-    ~cInfoContextCreator();
+    cInfoContextCreator() = default;
+    ~cInfoContextCreator() = default;
 
-    std::unique_ptr<cParticleInfos> createcParticleInfos();
+    std::unique_ptr<cParticleInfos> createParticleInfos();
     std::unique_ptr<cStructureInfos> createStructureInfos();
     std::unique_ptr<cBulletInfos> createBulletInfos();
     std::unique_ptr<cSpecialInfos> createSpecialInfos();
@@ -24,10 +23,10 @@ public:
     std::unique_ptr<cUnitInfos> createUnitInfos();
 
 private:
-    std::unique_ptr<cParticleInfos> m_particleInfos;
-    std::unique_ptr<cStructureInfos> m_structureInfos;
-    std::unique_ptr<cBulletInfos> m_bulletInfos;
-    std::unique_ptr<cSpecialInfos> m_specialInfos;
-    std::unique_ptr<cUpgradeInfos> m_upgradeInfos;
-    std::unique_ptr<cUnitInfos> m_unitInfos;
+    void initUnits(cUnitInfos& unitInfos);
+    void initStructures(cStructureInfos& structureInfos);
+    void initBullets(cBulletInfos& bulletInfos);
+    void initSpecials(cSpecialInfos& specialInfos);
+    void initParticles(cParticleInfos& particleInfos);
+    void initUpgrades(cUpgradeInfos& upgradeInfos);
 };
