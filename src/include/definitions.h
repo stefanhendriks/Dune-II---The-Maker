231ea7ecbdf49b95b7f691dc805b50ae3327f961
Mira <mira@legendes-oubliees.org>
2026-04-07 18:49:56 +0200

    Move WINLOSEFLAGS_XXXX to right destination

--- src/include/definitions.h
+++ src/include/definitions.h
@@ -249,10 +249,10 @@ const int D2TM_PARTICLE_SMOKE_WITH_SHADOW       = 35;
 // #define SMUDGE_WALL 2
 
 // Win/lose bit flags, borrowed from Dune Legacy's implementation
-#define WINLOSEFLAGS_AI_NO_BUILDINGS        0x01
-#define WINLOSEFLAGS_HUMAN_HAS_BUILDINGS    0x02
-#define WINLOSEFLAGS_QUOTA                  0x04
-#define WINLOSEFLAGS_TIMEOUT                0x08
+// #define WINLOSEFLAGS_AI_NO_BUILDINGS        0x01
+// #define WINLOSEFLAGS_HUMAN_HAS_BUILDINGS    0x02
+// #define WINLOSEFLAGS_QUOTA                  0x04
+// #define WINLOSEFLAGS_TIMEOUT                0x08
 
 
 /**
