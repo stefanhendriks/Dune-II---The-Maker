7dd12f07f124f6f3fcb386d497e51af15b70ab0a
Mira <mira@legendes-oubliees.org>
2026-04-08 18:54:07 +0200

    Better separation of responsibilities

--- src/context/cGameObjectContext.cpp
+++ src/context/cGameObjectContext.cpp
@@ -9,21 +9,28 @@
 
 #include <stdexcept>
 
-cGameObjectContext::~cGameObjectContext() = default;
-
-void cGameObjectContext::setBullets(std::unique_ptr<cBullets> bullets) {
-    m_Bullets = std::move(bullets);
+cGameObjectContext::cGameObjectContext(
+    std::unique_ptr<cBullets> bullets,
+    std::unique_ptr<cPlayers> players,
+    std::unique_ptr<cParticles> particles,
+    std::unique_ptr<cStructures> structures,
+    std::unique_ptr<cUnits> units,
+    std::unique_ptr<cMap> map)
+    : m_Bullets(std::move(bullets))
+    , m_Players(std::move(players))
+    , m_particles(std::move(particles))
+    , m_pStructures(std::move(structures))
+    , m_Units(std::move(units))
+    , m_map(std::move(map)) {
 }
+
+cGameObjectContext::~cGameObjectContext() = default;
 
 cBullets& cGameObjectContext::getBullets() const {
     if (!m_Bullets) {
         throw std::runtime_error("cBullets not initialized in cGameObjectContext");
     }
     return *m_Bullets;
-}
-
-void cGameObjectContext::setPlayers(std::unique_ptr<cPlayers> players) {
-    m_Players = std::move(players);
 }
 
 cPlayers& cGameObjectContext::getPlayers() const {
@@ -33,19 +40,11 @@ cPlayers& cGameObjectContext::getPlayers() const {
     return *m_Players;
 }
 
-void cGameObjectContext::setParticles(std::unique_ptr<cParticles> particles) {
-    m_particles = std::move(particles);
-}
-
 cParticles& cGameObjectContext::getParticles() const {
     if (!m_particles) {
         throw std::runtime_error("cParticles not initialized in cGameObjectContext");
     }
     return *m_particles;
-}
-
-void cGameObjectContext::setStructures(std::unique_ptr<cStructures> structures) {
-    m_pStructures = std::move(structures);
 }
 
 cStructures& cGameObjectContext::getStructures() const {
@@ -55,19 +54,11 @@ cStructures& cGameObjectContext::getStructures() const {
     return *m_pStructures;
 }
 
-void cGameObjectContext::setUnits(std::unique_ptr<cUnits> units) {
-    m_Units = std::move(units);
-}
-
 cUnits& cGameObjectContext::getUnits() const {
     if (!m_Units) {
         throw std::runtime_error("cUnits not initialized in cGameObjectContext");
     }
     return *m_Units;
-}
-
-void cGameObjectContext::setMap(std::unique_ptr<cMap> map) {
-    m_map = std::move(map);
 }
 
 cMap& cGameObjectContext::getMap() const {
