dcb2f7b1ea2f10dd3fab8590e19cb5bce2101d9d
Mira <mira@legendes-oubliees.org>
2026-04-15 19:13:47 +0200

    Creating GameInterface

---
+++ src/game/cGameInterface.cpp
@@ -0,0 +1,34 @@
+#include "game/cGameInterface.h"
+#include "data/gfxaudio.h"
+#include "game/cGame.h"
+#include "utils/cLog.h"
+
+#include <format>
+
+cGameInterface::cGameInterface(cGame* game)
+    : m_igame(game)
+{
+}
+
+void cGameInterface::prepareMentatToTellAboutHouse(int house)
+{
+    switch(house) {
+    case ATREIDES:
+            m_igame->prepareMentatToTellAboutHouse(ATREIDES);
+            m_igame->playSound(SOUND_ATREIDES);
+            break;
+    case HARKONNEN:
+            m_igame->prepareMentatToTellAboutHouse(HARKONNEN);
+            m_igame->playSound(SOUND_HARKONNEN);
+            break;
+    case ORDOS:
+            m_igame->prepareMentatToTellAboutHouse(ORDOS);
+            m_igame->playSound(SOUND_ORDOS);
+            break;
+    default:
+        cLogger::getInstance()->log(LOG_WARN,COMP_GAMERULES, "House", std::format("cGameInterface::prepareMentatToTellAboutHouse() was called with an invalid house value: {}", house));
+        break;
+    }
+    m_igame->setNextStateToTransitionTo(GAME_TELLHOUSE);
+    m_igame->initiateFadingOut();
+}
