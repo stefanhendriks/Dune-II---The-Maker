b85af932923f4a42569bb1c31818c7ff50d44905
Mira <mira@legendes-oubliees.org>
2026-04-16 18:06:43 +0200

    Fix const

--- src/game/cGameInterface.h
+++ src/game/cGameInterface.h
@@ -8,7 +8,7 @@ public:
     cGameInterface(cGame* game);
     virtual ~cGameInterface() = default;
 
-    void prepareMentatToTellAboutHouse(int house);
+    void prepareMentatToTellAboutHouse(int house) const;
 
 private:
     cGame* m_igame = nullptr;
