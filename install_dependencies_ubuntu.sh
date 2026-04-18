2826b072f1e77cba60ea1b2e038fe95116f0d70a
Stefan Hendriks <stefan@fundynamic.com>
2026-04-18 09:56:10 +0200

    Add dependency in scripts

--- install_dependencies_ubuntu.sh
+++ install_dependencies_ubuntu.sh
@@ -11,4 +11,4 @@ echo  ---------------------------------
 
 sudo apt-get install g++
 sudo apt-get install make cmake
-sudo apt-get install libsdl2-dev libsdl2-image-dev libsdl2-ttf-dev libsdl2-mixer-dev
+sudo apt-get install libsdl2-dev libsdl2-image-dev libsdl2-ttf-dev libsdl2-mixer-dev catch2
