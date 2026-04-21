dd354ea51f21a73d9185010965892f259158aa6c
Mira <mira@legendes-oubliees.org>
2026-04-21 15:47:38 +0200

    Access MapGeometry

--- src/context/cGameObjectContext.h
+++ src/context/cGameObjectContext.h
@@ -14,6 +14,7 @@ class cPlayer;
 class cUnit;
 class cAbstractStructure;
 class cStructureFactory;
+class MapGeometry;
 
 struct sGameServices;
 
@@ -31,6 +32,7 @@ public:
 
     cBullets& getBullets() const;
     cMap& getMap() const;
+    MapGeometry* getMapGeometry() const;
     cPlayers* getPlayers() const;
     cParticles& getParticles() const;
 
