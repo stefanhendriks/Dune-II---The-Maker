707076be4d7ee2e49ca57651e10df5ac4e9a4a5f
Mira <mira@legendes-oubliees.org>
2026-04-23 19:09:17 +0200

    Using DeployUnitEvent

--- src/game/cGame.h
+++ src/game/cGame.h
@@ -163,7 +163,7 @@ public:
 
     void onEventSpecialLaunch(const s_GameEvent &event) const;
     void onEventEntityDestroyed(const s_GameEvent & event);
-    void onEventCreateUnit(const s_GameEvent &event);
+    void onEventCreateUnit(const DeployUnitEvent &deployEvent);
 
     void shakeScreen(int duration);
     void reduceShaking() const;
