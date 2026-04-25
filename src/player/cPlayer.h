bf0b5d03ee81d4e25cbf4deeb818b92ba2c4a6aa
Mira <mira@legendes-oubliees.org>
2026-04-25 08:36:48 +0200

    cPlayer can search all my Infantry

--- src/player/cPlayer.h
+++ src/player/cPlayer.h
@@ -314,6 +314,8 @@ public:
 
     std::vector<int> getAllMyUnitsForType(int unitType) const;
 
+    std::vector<int> getAllMyInfantryUnits() const;
+
     std::vector<int> getAllMyUnitsWithinViewportRect(const cRectangle &rect) const;
 
     std::vector<int> getAllMyStructuresAsId();
