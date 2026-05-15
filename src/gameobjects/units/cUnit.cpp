ae8c5401ae9ff09bccaa65006a162f3ea2ee55ea
Mira <mira@legendes-oubliees.org>
2026-05-15 23:10:13 +0200

    Duplicate function

--- src/gameobjects/units/cUnit.cpp
+++ src/gameobjects/units/cUnit.cpp
@@ -3117,10 +3117,10 @@ int cUnit::pos_x_centered()
  * Clears the created path, resets next-cell to current cell. Sets timer to wait to 100. So that
  * after that the unit will think of a new path to create.
  */
-void cUnit::forgetAboutCurrentPathAndPrepareToCreateNewOne()
-{
-    forgetAboutCurrentPathAndPrepareToCreateNewOne(35);
-}
+// void cUnit::forgetAboutCurrentPathAndPrepareToCreateNewOne()
+// {
+//     forgetAboutCurrentPathAndPrepareToCreateNewOne(35);
+// }
 
 /**
  * Clears the created path, resets next-cell to current cell. Sets timer to wait to <timeToWait>. So that
