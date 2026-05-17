46c25006ac2a7bab8d38860c6e79189ef841c292
Stefan Hendriks <stefan@fundynamic.com>
2026-05-17 11:29:51 +0200

    Render cheat mode enabled/disabled notification in green

--- src/include/eNotificationType.h
+++ src/include/eNotificationType.h
@@ -3,5 +3,6 @@
 enum eNotificationType {
     NEUTRAL,    // generic message, in white
     PRIORITY,   // a more important message, yellow
-    BAD         // bad news, in red
+    BAD,        // bad news, in red
+    OTHER       // other kind of messages
 };
