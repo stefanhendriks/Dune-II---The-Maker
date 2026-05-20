b769c694e50a71402bfc3a54c36d1ad5888e4472
Mira <mira@legendes-oubliees.org>
2026-05-21 00:10:27 +0200

    Extract handleEvent from cGame

--- src/game/cGame_logic.cpp
+++ src/game/cGame_logic.cpp
@@ -22,6 +22,7 @@
 #include "include/eGameState.h"
 
 #include "game/cGameConditionChecker.h"
+#include "game/cGameEventHandler.h"
 #include "game/cPlatformLayerInit.h"
 #include "game/cScreenFader.h"
 #include "game/cScreenInit.h"
@@ -192,6 +193,20 @@ cGame::cGame()
 
     m_sideBarFactory = std::make_unique<cSideBarFactory>();
     m_buildingListFactory = std::make_unique<cBuildingListFactory>();
+
+    m_gameEventHandler = std::make_unique<cGameEventHandler>(
+        m_gameObjectsContext.get(),
+        m_infoContext.get(),
+        m_structureUtils.get(),
+        [this](int sampleId) {
+            if (m_soundPlayer) {
+                m_soundPlayer->playSound(sampleId);
+            }
+        },
+        [this](const s_GameEvent &event) {
+            onNotifyGameEvent(event);
+        }
+    );
 
     m_services = std::make_unique<sGameServices>();
     m_services->ctx = ctx.get();
@@ -1148,26 +1163,13 @@ void cGame::onNotifyGameEvent(const s_GameEvent &event)
 
     m_gameObjectsContext->getMap()->onNotifyGameEvent(event);
 
+    m_gameEventHandler->handleEvent(event);
+
     // game itself handles events
     switch (event.eventType) {
         case eGameEventType::GAME_EVENT_NOTIFICATION:
             if (const auto *notifEvent = std::get_if<NotificationEvent>(&event.data)) {
                 m_notificationArea->addNotification(notifEvent->message, notifEvent->type);
-            }
-            break;
-        case eGameEventType::GAME_EVENT_SPECIAL_LAUNCH:
-            if (const auto *launchEvent = std::get_if<LaunchDeathHandEvent>(&event.data)) {
-                onEventSpecialLaunch(*launchEvent);
-            }
-            break;
-        case eGameEventType::GAME_EVENT_DESTROYED:
-            if (const auto *destroyEvent = std::get_if<CommonEvent>(&event.data)) {
-                onEventEntityDestroyed(*destroyEvent);
-            }
-            break;
-        case eGameEventType::GAME_EVENT_CREATE_UNIT:
-            if (const auto *deployEvent = std::get_if<DeployUnitEvent>(&event.data)) {
-                onEventCreateUnit(*deployEvent);
             }
             break;
         default:
@@ -1176,146 +1178,6 @@ void cGame::onNotifyGameEvent(const s_GameEvent &event)
 
     // players handle events
     m_players->onNotifyGameEvent(event);
-}
-
-void cGame::onEventEntityDestroyed(const CommonEvent &event)
-{
-    if (event.entityType != eBuildType::STRUCTURE) {
-        return;
-    }
-
-    const auto structureInfo = (*m_infoContext->getStructureInfos())[event.entitySpecificType];
-    int unitTypeToSpawn = structureInfo.uponDestructionSpawnUnitType;
-    if (unitTypeToSpawn < 0) {
-        return;
-    }
-
-    int minAmountOfUnitsToSpawnPotentially = structureInfo.uponDestructionSpawnUnitAmountMin;
-    int maxAmountOfUnitsToSpawnPotentially = structureInfo.uponDestructionSpawnUnitAmountMax;
-
-    int amountOfUnitsToSpawn = RNG::genInt(minAmountOfUnitsToSpawnPotentially, maxAmountOfUnitsToSpawnPotentially);
-
-    int widthInCells = structureInfo.bmp_width / 32;
-    int heightInCells = structureInfo.bmp_height / 32;
-
-    int cellX = m_gameObjectsContext->getMapGeometry()->getCellX(event.atCell);
-    int cellY = m_gameObjectsContext->getMapGeometry()->getCellY(event.atCell);
-
-    for (int i = 0; i < amountOfUnitsToSpawn; i++) {
-        int randomX = cellX + RNG::genIntMaxExcl(0, widthInCells);
-        int randomY = cellY + RNG::genIntMaxExcl(0, heightInCells);
-        cUnits::unitCreate(
-            m_gameObjectsContext->getMapGeometry()->makeCell(randomX, randomY),
-            unitTypeToSpawn,
-            event.player->getId(),
-            false,
-            false,
-            RNG::genDouble(0.3, 0.8)
-        );
-    }
-}
-
-void cGame::onEventCreateUnit(const DeployUnitEvent &event) {
-    if (event.iCell < 0 || event.unitType < 0 || event.iPlayer < 0) {
-        return;
-    }
-    int id = cUnits::unitCreate(
-        event.iCell,
-        event.unitType,
-        event.iPlayer,
-        event.bOnStart,
-        event.isReinforcement,
-        event.hpPercentage
-    );
-
-    if (id < 0) {
-        cLogger::getInstance()->log( LOG_ERROR, COMP_GAME, "Deploy unit",
-            std::format("Failed to deploy unit of type {} at cell {} for player {}", event.unitType, event.iCell, event.iPlayer)
-        );
-    }
-    else {
-        cLogger::getInstance()->log( LOG_INFO, COMP_GAME, "Deploy unit",
-            std::format("Successfully deployed unit of type {} at cell {} for player {}, id={}",event.unitType, event.iCell, event.iPlayer, id )
-        );
-    }
-}
-
-void cGame::onEventSpecialLaunch(const LaunchDeathHandEvent &event) const
-{
-    cBuildingListItem *itemToDeploy = event.itemToLaunch;
-    int iMouseCell = event.targetCell;
-    cPlayer *player = event.player;
-    if (itemToDeploy->isTypeSpecial()) {
-        const s_SpecialInfo &special = itemToDeploy->getSpecialInfo();
-
-        int deployCell = -1;
-        if (special.deployTargetType == eDeployTargetType::TARGET_SPECIFIC_CELL) {
-            deployCell = iMouseCell;
-        }
-        else if (special.deployTargetType == eDeployTargetType::TARGET_INACCURATE_CELL) {
-            int precision = special.deployTargetPrecision;
-            int mouseCellX = m_gameObjectsContext->getMapGeometry()->getCellX(iMouseCell) - precision;
-            int mouseCellY = m_gameObjectsContext->getMapGeometry()->getCellY(iMouseCell) - precision;
-
-            int posX = mouseCellX + RNG::rnd((precision * 2) + 1);
-            int posY = mouseCellY + RNG::rnd((precision * 2) + 1);
-            cPoint::split(posX, posY) = m_gameObjectsContext->getMapGeometry()->fixCoordinatesToBeWithinPlayableMap(posX, posY);
-
-            logbook(std::format(
-                        "eDeployTargetType::TARGET_INACCURATE_CELL, mouse cell X,Y = {},{} - target pos ={},{} - precision {}",
-                        mouseCellY, mouseCellY, posX, posY,precision)
-                   );
-
-            deployCell = m_gameObjectsContext->getMapGeometry()->makeCell(posX, posY);
-        }
-
-
-        if (special.providesType == eBuildType::BULLET) {
-            // from where
-            int structureId = m_structureUtils->findStructureBy(player->getId(), special.deployAtStructure, false);
-            if (structureId > -1) {
-                cAbstractStructure *pStructure = m_gameObjectsContext->getStructures()[structureId];
-                if (pStructure && pStructure->isValid()) {
-                    m_soundPlayer->playSound(SOUND_PLACE);
-                    createBullet(special.providesTypeId, pStructure->getCell(), deployCell, -1, structureId);
-
-                    // notify game that the item just has been finished!
-                    const s_GameEvent event {
-                        .eventType = eGameEventType::GAME_EVENT_SPECIAL_LAUNCHED,
-                        .data = BuildingEvent {
-                            .entityType = itemToDeploy->getBuildType(),
-                            .player = pStructure->getPlayer(),
-                            .entitySpecificType = itemToDeploy->getBuildId(),
-                            .buildingListItem = itemToDeploy
-                        }
-                    };
-                    game.onNotifyGameEvent(event);
-                }
-            }
-        }
-    }
-
-    itemToDeploy->decreaseTimesToBuild();
-    itemToDeploy->setDeployIt(false);
-    itemToDeploy->cancelBuilding();
-    if (itemToDeploy->getTimesToBuild() < 1) {
-        player->getItemBuilder()->removeItemFromList(itemToDeploy);
-    }
-
-    if (player) {
-        player->getGameControlsContext()->toPreviousState();
-    }
-
-    // notify game that the item just has been finished!
-    const s_GameEvent eventT {
-        .eventType = eGameEventType::GAME_EVENT_LIST_ITEM_FINISHED,
-        .data = BuildingEvent {
-            .entityType = itemToDeploy->getBuildType(),
-            .player = player,
-            .entitySpecificType = itemToDeploy->getBuildId()
-        }
-    };
-    game.onNotifyGameEvent(eventT);
 }
 
 void cGame::reduceShaking() const {
