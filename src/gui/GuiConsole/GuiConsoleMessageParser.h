49bd57492414ede5e786ac4755e516a70a589bc1
Mira <mira@legendes-oubliees.org>
2026-05-20 06:41:19 +0200

    Order preparation and analysis

--- src/gui/GuiConsole/GuiConsoleMessageParser.h
+++ src/gui/GuiConsole/GuiConsoleMessageParser.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include <string>
+#include <map>
+
+typedef std::map< std::string, std::string > stringHash;
 
 class cNotificationArea;
 
@@ -8,10 +11,15 @@ class GuiConsoleMessageParser {
 public:
     explicit GuiConsoleMessageParser(cNotificationArea* notificationArea);
 
-    void submit(const std::string& text) const;
+    void submit(const std::string& text);
 
 private:
+    void parseCommand(const std::string& text);
+    bool verifyOddWordCommand(const std::string& text) const;
+
     std::string normalize(const std::string& text) const;
 
     cNotificationArea* m_notificationArea = nullptr;
+    std::string m_command;
+    stringHash m_arguments;
 };
