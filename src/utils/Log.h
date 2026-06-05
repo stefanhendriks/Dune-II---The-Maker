c43faabaa1f0a8e83974c8c6d396b38d69c5564b
Mira <mira@legendes-oubliees.org>
2026-06-05 06:13:37 +0200

    Template integration

--- src/utils/Log.h
+++ src/utils/Log.h
@@ -7,6 +7,7 @@
 #include <cstddef>
 #include <fstream>
 #include <string_view>
+#include <chrono>
 
 class cLog {
 public:
@@ -19,6 +20,8 @@ public:
     cLog &operator=(cLog &&) = delete;
 
     void log(eLogLevel level, eLogComponent component, std::string_view event, std::string_view message, int playerId = -1, int houseId = -1);
+
+    void print(eLogLevel level, eLogComponent component, std::string_view event, std::string_view message);
 
     template <typename... Args>
     void error(eLogComponent comp, std::string_view event, std::format_string<Args...> fmt, Args&&... args);
@@ -43,6 +46,7 @@ private:
     void flushPendingLine(bool forceFlush);
 
     std::ofstream m_file;
+    std::chrono::steady_clock::time_point m_startTime;
     std::string m_pendingPayload;
     long long m_pendingTimestampMs = 0;
     std::size_t m_pendingCount = 0;
@@ -50,4 +54,90 @@ private:
     bool m_debugMode = false;
 };
 
-#include "utils/Log.tpp"
+// ============================================================================
+// NAMESPACE GLOBAL DECLARATION (INTERFACE LOG)
+// ============================================================================
+namespace Logger {
+    // Initialise le système avec l'instance de Log créée dans le main
+    void init(cLog* instance);
+
+    // Surcharges explicites si tu veux spécifier le composant et l'événement
+    template <typename... Args>
+    void print(eLogLevel level, eLogComponent comp, std::string_view event, std::format_string<Args...> fmt, Args&&... args);
+    template <typename... Args>
+    void info(eLogComponent comp, std::string_view event, std::format_string<Args...> fmt, Args&&... args);
+    template <typename... Args>
+    void trace(eLogComponent comp, std::string_view event, std::format_string<Args...> fmt, Args&&... args);
+    template <typename... Args>
+    void warn(eLogComponent comp, std::string_view event, std::format_string<Args...> fmt, Args&&... args);
+    template <typename... Args>
+    void error(eLogComponent comp, std::string_view event, std::format_string<Args...> fmt, Args&&... args);
+    template <typename... Args>
+    void fatal(eLogComponent comp, std::string_view event, std::format_string<Args...> fmt, Args&&... args);
+}
+
+
+// ============================================================================
+// ============================================================================
+// TEMPLATES IMPLEMENTATION  (HEADER REQUIREMENT)
+// ============================================================================
+// ============================================================================
+#include <utility>
+
+// --- Methodes from Log class ---
+template <typename... Args>
+void cLog::error(eLogComponent comp, std::string_view event, std::format_string<Args...> fmt, Args&&... args) {
+    doLog(LOG_ERROR, comp, event, std::format(fmt, std::forward<Args>(args)...));
+}
+
+template <typename... Args>
+void cLog::warn(eLogComponent comp, std::string_view event, std::format_string<Args...> fmt, Args&&... args) {
+    doLog(LOG_WARN, comp, event, std::format(fmt, std::forward<Args>(args)...));
+}
+
+template <typename... Args>
+void cLog::trace(eLogComponent comp, std::string_view event, std::format_string<Args...> fmt, Args&&... args) {
+    doLog(LOG_TRACE, comp, event, std::format(fmt, std::forward<Args>(args)...));
+}
+
+template <typename... Args>
+void cLog::fatal(eLogComponent comp, std::string_view event, std::format_string<Args...> fmt, Args&&... args) {
+    doLog(LOG_FATAL, comp, event, std::format(fmt, std::forward<Args>(args)...));
+}
+
+template <typename... Args>
+void cLog::info(eLogComponent comp, std::string_view event, std::format_string<Args...> fmt, Args&&... args) {
+    doLog(LOG_INFO, comp, event, std::format(fmt, std::forward<Args>(args)...));
+}
+
+// --- Globales fonctions from Namespace Logger ---
+namespace Logger {
+    // On indique au header que cette variable existe dans le .cpp
+    extern cLog* g_loggerInstance;
+
+    // Versions complètes avec Composant et Événement
+    template <typename... Args>
+    void print(eLogLevel level, eLogComponent comp, std::string_view event, std::format_string<Args...> fmt, Args&&... args) {
+        if (g_loggerInstance) g_loggerInstance->print(level, comp, event, std::format(fmt, std::forward<Args>(args)...));
+    }
+    template <typename... Args>
+    void info(eLogComponent comp, std::string_view event, std::format_string<Args...> fmt, Args&&... args) {
+        if (g_loggerInstance) g_loggerInstance->info(comp, event, fmt, std::forward<Args>(args)...);
+    }
+    template <typename... Args>
+    void trace(eLogComponent comp, std::string_view event, std::format_string<Args...> fmt, Args&&... args) {
+        if (g_loggerInstance) g_loggerInstance->trace(comp, event, fmt, std::forward<Args>(args)...);
+    }
+    template <typename... Args>
+    void warn(eLogComponent comp, std::string_view event, std::format_string<Args...> fmt, Args&&... args) {
+        if (g_loggerInstance) g_loggerInstance->warn(comp, event, fmt, std::forward<Args>(args)...);
+    }
+    template <typename... Args>
+    void error(eLogComponent comp, std::string_view event, std::format_string<Args...> fmt, Args&&... args) {
+        if (g_loggerInstance) g_loggerInstance->error(comp, event, fmt, std::forward<Args>(args)...);
+    }
+    template <typename... Args>
+    void fatal(eLogComponent comp, std::string_view event, std::format_string<Args...> fmt, Args&&... args) {
+        if (g_loggerInstance) g_loggerInstance->fatal(comp, event, fmt, std::forward<Args>(args)...);
+    }
+}
