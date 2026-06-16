83faef82864c66b4a4fa0da4039b24aa61b0c383
Stefan Hendriks <stefan@fundynamic.com>
2026-06-16 14:14:20 +0200

    Button render cycling via right-click restricted to debug mode
    Right-clicking a focused GuiButton while in debug mode cycles through
    render kinds (useful for layout inspection). Commit 6f9726dd removed
    this feature entirely; restore it behind the isDebugMode() guard.
    
    A static cGameSettings pointer on GuiButton avoids re-coupling the
    class to the global game instance. It is registered from applySettings()
    once settings are known.
    
    Fixes #1036

--- src/gui/GuiButton.h
+++ src/gui/GuiButton.h
@@ -9,6 +9,7 @@
 
 class SDLDrawer;
 class cTextDrawer;
+class cGameSettings;
 
 
 class GuiButton : public GuiObject {
@@ -37,7 +38,10 @@ public:
 
     void setEnabled(bool value);
 
+    static void registerSettings(cGameSettings *settings);
+
 private:
+    static cGameSettings *s_settings;
     cTextDrawer *m_textDrawer;
     std::string m_buttonText;
     GuiRenderKind m_renderKind;
